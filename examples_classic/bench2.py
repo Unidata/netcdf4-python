b64c79ecd97dff8f49a24ea32b79af74edb6d702
Jeff Whitaker <jswhit@fastmail.fm>
2006-09-19 11:37:46 +0000

    use timeit with 10 trials for more accurate comparison with Scientific.IO.NetCDF

--- examples_classic/bench2.py
+++ examples_classic/bench2.py
@@ -3,17 +3,19 @@
 from numpy.random.mtrand import uniform
 import pynetcdf
 import netCDF4_classic
-import sys, time, os
+from timeit import Timer
 
 # create an n1dim by n2dim by n3dim random array.
 n1dim = 30   
 n2dim = 15
 n3dim = 73
 n4dim = 144
+ntrials = 10
 print 'reading and writing a %s by %s by %s by %s random array ..'%(n1dim,n2dim,n3dim,n4dim)
+print '(average of %s trials)' % ntrials
 array = uniform(size=(n1dim,n2dim,n3dim,n4dim))
 
-def write_pynetcdf(array,filename,n1dim,n2dim,n3dim,n4dim,zlib=False,complevel=0,shuffle=0,least_significant_digit=None):
+def write_pynetcdf(filename):
     file = pynetcdf.NetCDFFile(filename,'w')
     file.createDimension('n1', n1dim)
     file.createDimension('n2', n2dim)
@@ -23,13 +25,13 @@ def write_pynetcdf(array,filename,n1dim,n2dim,n3dim,n4dim,zlib=False,complevel=0
     foo[:] = array
     file.close()
 
-def write_netcdf4(array,filename,n1dim,n2dim,n3dim,n4dim,zlib=False,complevel=0,shuffle=0,least_significant_digit=None):
+def write_netcdf4(filename):
     file = netCDF4_classic.Dataset(filename,'w')
     file.createDimension('n1', n1dim)
     file.createDimension('n2', n2dim)
     file.createDimension('n3', n3dim)
     file.createDimension('n4', n4dim)
-    foo = file.createVariable('data', 'f8',('n1','n2','n3','n4'),zlib=False)
+    foo = file.createVariable('data', 'd',('n1','n2','n3','n4'),zlib=False)
     foo[:] = array
     file.close()
 
@@ -44,16 +46,12 @@ def read_netcdf4(filename):
     file.close()
 
 # test writing.
-t1 = time.time()
-write_pynetcdf(array,'test_pynetcdf.nc',n1dim,n2dim,n3dim,n4dim)
-print 'writing with pynetcdf took',time.time()-t1,'seconds'
-t1 = time.time()
-write_netcdf4(array,'test_netcdf4.nc',n1dim,n2dim,n3dim,n4dim)
-print 'writing with netCDF4_classic took',time.time()-t1,'seconds'
+t = Timer("write_pynetcdf('test_pynetcdf.nc')","from __main__ import write_pynetcdf, array, n1dim, n2dim, n3dim, n4dim")
+print 'writing with pynetcdf took',sum(t.repeat(ntrials,1))/ntrials,'seconds'
+t = Timer("write_netcdf4('test_netcdf4.nc')","from __main__ import write_netcdf4, array, n1dim, n2dim, n3dim, n4dim")
+print 'writing with netCDF4_classic took',sum(t.repeat(ntrials,1))/ntrials,'seconds'
 # test reading.
-t1 = time.time()
-read_pynetcdf('test_pynetcdf.nc')
-print 'reading with pynetcdf took',time.time()-t1,'seconds to read'
-t1 = time.time()
-read_netcdf4('test_netcdf4.nc')
-print 'reading with netCDF4_classic file took',time.time()-t1,'seconds to read'
+t = Timer("read_pynetcdf('test_pynetcdf.nc')","from __main__ import read_pynetcdf")
+print 'reading with pynetcdf took',sum(t.repeat(ntrials,1))/ntrials,'seconds'
+t = Timer("read_netcdf4('test_netcdf4.nc')","from __main__ import read_netcdf4")
+print 'reading with netCDF4_classic took',sum(t.repeat(ntrials,1))/ntrials,'seconds'
