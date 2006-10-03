bb0ade803f3ae475fb81ac75afd8bb5098e2986d
Jeff Whitaker <jswhit@fastmail.fm>
2006-10-03 16:33:19 +0000

    update docs

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,2 @@
 # svn propset svn:mime-type text/html docs/*html
-epydoc -v --no-frames -o docs MFDataset.MFDataset netCDF4_classic netCDF4 netcdftime.netcdftime 
+epydoc -v --no-frames --introspect-only -o docs netCDF4_classic netCDF4 netcdftime.netcdftime MFDataset.MFDataset
