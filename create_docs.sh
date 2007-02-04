2ac3062ab81e153acdbf057a16cb34424e86348c
Jeff Whitaker <jswhit@fastmail.fm>
2007-02-04 16:43:59 +0000

    renamed MFDataset

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,2 @@
 # svn propset svn:mime-type text/html docs/*html
-epydoc -v --no-frames --introspect-only -o docs netCDF4_classic netCDF4 netcdftime.netcdftime MFDataset.MFDataset
+epydoc -v --no-frames --introspect-only -o docs netCDF4_classic netCDF4 netcdftime.netcdftime MFnetCDF4_classic.MFnetCDF4_classic
