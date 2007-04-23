0a5a09b50134385b85f84b14fdc5e7b0cc07081c
Jeff Whitaker <jswhit@fastmail.fm>
2007-04-23 18:42:51 +0000

    fixed hdf5 URL

--- netCDF4_classic.pyx
+++ netCDF4_classic.pyx
@@ -30,7 +30,7 @@ Requires
  If you're using python 2.5, you'll need at least version 0.9.5.
  - numpy array module U{http://numpy.scipy.org}, version 1.0 or later.
  - The HDF5 C library (version 1.8.0-beta1 recommended),  
- available at U{ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/hdf5/hdf5-1.8.0-pre/}.
+ available at U{ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/hdf5/hdf5-1.8.0-pre}.
  Be sure to build with 'C{--enable-hl}'.
  - The netCDF-4 C library (version 4.0-beta1 recommended), available at
  U{ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4}.
