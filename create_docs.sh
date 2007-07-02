e1fb3a6689f23444357530d379aa203ce801b926
Jeff Whitaker <jswhit@fastmail.fm>
2007-07-02 20:16:47 +0000

    don't show private variables by default.

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,2 @@
 # svn propset svn:mime-type text/html docs/*html
-epydoc -v --no-frames --introspect-only -o docs netCDF4_classic netCDF4 netcdftime.netcdftime MFnetCDF4_classic.MFnetCDF4_classic
+epydoc -v --no-frames --no-private --introspect-only -o docs netCDF4_classic netCDF4 netcdftime.netcdftime MFnetCDF4_classic.MFnetCDF4_classic
