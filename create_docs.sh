165151798ba450acf13851cd3fed1f4077e93b43
Jeff Whitaker <jswhit@fastmail.fm>
2007-12-27 13:53:28 +0000

    add netCDF3

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,2 @@
 # svn propset svn:mime-type text/html docs/*html
-epydoc -v --no-frames --no-private --introspect-only -o docs netCDF4 netcdftime.netcdftime MFnetCDF4.MFnetCDF4
+epydoc -v --no-frames --no-private --introspect-only -o docs netCDF4 netCDF3 netcdftime.netcdftime MFnetCDF4.MFnetCDF4
