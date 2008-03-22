f1a8ea649b71709764626c8971eda6dbaaefa807
Jeff Whitaker <jswhit@fastmail.fm>
2008-03-22 12:19:07 +0000

    fixed URLs in netcdftime.py copied date2num, num2date to netcdftime.py regenerated docs, adding netcdftime

--- netcdftime/netcdftime.py
+++ netcdftime/netcdftime.py
@@ -466,8 +466,8 @@ since 0001-01-01 00:00:00'}.
 
 The B{C{calendar}} keyword describes the calendar used in the time calculations. 
 All the values currently defined in the U{CF metadata convention 
-<http://www.cgd.ucar.edu/cms/eaton/cf-metadata/CF-1.0.html#time>} are 
-accepted. The default is C{'standard'}, which corresponds to the mixed 
+<http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.1/cf-conventions.html#time-coordinate>}  
+are accepted. The default is C{'standard'}, which corresponds to the mixed 
 Gregorian/Julian calendar used by the C{udunits library}. Valid calendars 
 are:
 
@@ -533,8 +533,7 @@ Warning:  Dates between 1582-10-5 and 1582-10-15 do not exist in the
 C{'standard'} or C{'gregorian'} calendars.  An exception will be raised if you pass 
 a 'datetime-like' object in that range to the C{L{date2num}} class method.
 
-Words of Wisdom from the British MetOffice concerning reference dates 
-U{http://www.metoffice.com/research/hadleycentre/models/GDT/ch26.html}:
+Words of Wisdom from the British MetOffice concerning reference dates:
 
 "udunits implements the mixed Gregorian/Julian calendar system, as 
 followed in England, in which dates prior to 1582-10-15 are assumed to use 
@@ -559,8 +558,8 @@ since 0001-01-01 00:00:00'}.
 
 @keyword calendar: describes the calendar used in the time calculations. 
 All the values currently defined in the U{CF metadata convention 
-<http://www.cgd.ucar.edu/cms/eaton/cf-metadata/CF-1.0.html#time>} are 
-accepted. The default is C{'standard'}, which corresponds to the mixed 
+<http://cf-pcmdi.llnl.gov/documents/cf-conventions/1.1/cf-conventions.html#time-coordinate>}
+are accepted. The default is C{'standard'}, which corresponds to the mixed 
 Gregorian/Julian calendar used by the C{udunits library}. Valid calendars 
 are:
  - C{'gregorian'} or C{'standard'} (default):
@@ -851,3 +850,86 @@ def _strftime(dt, fmt):
     for site in sites:
         s = s[:site] + syear + s[site+4:]
     return s
+
+def date2num(dates,units,calendar='standard'):
+    """
+date2num(dates,units,calendar='standard')
+
+Return numeric time values given datetime objects. The units
+of the numeric time values are described by the L{units} argument
+and the L{calendar} keyword. The datetime objects must
+be in UTC with no time-zone offset.  If there is a 
+time-zone offset in C{units}, it will be applied to the
+returned numeric values.
+
+Like the matplotlib C{date2num} function, except that it allows
+for different units and calendars.  Behaves the same if
+C{units = 'days since 0001-01-01 00:00:00'} and 
+C{calendar = 'proleptic_gregorian'}.
+
+@param dates: A datetime object or a sequence of datetime objects.
+ The datetime objects should not include a time-zone offset.
+
+@param units: a string of the form C{'B{time units} since B{reference time}}'
+ describing the time units. B{C{time units}} can be days, hours, minutes
+ or seconds.  B{C{reference time}} is the time origin. A valid choice
+ would be units=C{'hours since 1800-01-01 00:00:00 -6:00'}.
+
+@param calendar: describes the calendar used in the time calculations. 
+ All the values currently defined in the U{CF metadata convention 
+ <http://cf-pcmdi.llnl.gov/documents/cf-conventions/>} are supported.
+ Valid calendars C{'standard', 'gregorian', 'proleptic_gregorian'
+ 'noleap', '365_day', '360_day', 'julian', 'all_leap', '366_day'}.
+ Default is C{'standard'}, which is a mixed Julian/Gregorian calendar.
+
+@return: a numeric time value, or an array of numeric time values.
+
+The maximum resolution of the numeric time values is 1 second.
+    """
+    cdftime = utime(units,calendar=calendar)
+    return cdftime.date2num(dates)
+
+def num2date(times,units,calendar='standard'):
+    """
+num2date(times,units,calendar='standard')
+
+Return datetime objects given numeric time values. The units
+of the numeric time values are described by the C{units} argument
+and the C{calendar} keyword. The returned datetime objects represent 
+UTC with no time-zone offset, even if the specified 
+C{units} contain a time-zone offset.
+
+Like the matplotlib C{num2date} function, except that it allows
+for different units and calendars.  Behaves the same if
+C{units = 'days since 001-01-01 00:00:00'} and 
+C{calendar = 'proleptic_gregorian'}.
+
+@param times: numeric time values. Maximum resolution is 1 second.
+
+@param units: a string of the form C{'B{time units} since B{reference time}}'
+describing the time units. B{C{time units}} can be days, hours, minutes
+or seconds.  B{C{reference time}} is the time origin. A valid choice
+would be units=C{'hours since 1800-01-01 00:00:00 -6:00'}.
+
+@param calendar: describes the calendar used in the time calculations. 
+All the values currently defined in the U{CF metadata convention 
+<http://cf-pcmdi.llnl.gov/documents/cf-conventions/>} are supported.
+Valid calendars C{'standard', 'gregorian', 'proleptic_gregorian'
+'noleap', '365_day', '360_day', 'julian', 'all_leap', '366_day'}.
+Default is C{'standard'}, which is a mixed Julian/Gregorian calendar.
+
+@return: a datetime instance, or an array of datetime instances.
+
+The datetime instances returned are 'real' python datetime 
+objects if the date falls in the Gregorian calendar (i.e. 
+C{calendar='proleptic_gregorian'}, or C{calendar = 'standard'} or C{'gregorian'}
+and the date is after 1582-10-15). Otherwise, they are 'phony' datetime 
+objects which support some but not all the methods of 'real' python
+datetime objects.  This is because the python datetime module cannot
+the uses the C{'proleptic_gregorian'} calendar, even before the switch
+occured from the Julian calendar in 1582. The datetime instances
+do not contain a time-zone offset, even if the specified C{units}
+contains one.
+    """
+    cdftime = utime(units,calendar=calendar)
+    return cdftime.num2date(times)
