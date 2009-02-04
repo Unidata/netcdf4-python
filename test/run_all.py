14c1f215de1e921cf102400439f1063d4cc6c36a
David Huard <david.huard@gmail.com>
2009-02-04 17:57:26 +0000

    modified run_all to use unittest.testsuite: test results from all tests are now merged.

--- test/run_all.py
+++ test/run_all.py
@@ -1,4 +1,6 @@
-import glob, os, sys
+import glob, os, sys, unittest
+
+# Find all test files.
 test_files = glob.glob('tst_*.py')
 py_path = os.environ.get('PYTHONPATH')
 if py_path is None:
@@ -7,10 +9,13 @@ else:
     py_path = os.pathsep.join(['.',py_path])
 os.environ['PYTHONPATH'] = py_path
 
+# Build the test suite from the tests found in the test files.
+testsuite = unittest.TestSuite()
 for f in test_files:
     ff = os.path.join(sys.path[0],f)
-    args = [sys.executable,ff,'-v']
-    print "Running",ff
-    status = os.spawnve(os.P_WAIT,sys.executable,args,os.environ)
-    if status:
-        print 'TEST FAILURE (status=%s)' % (status)
+    m = __import__(os.path.splitext(f)[0])
+    testsuite.addTests(unittest.TestLoader().loadTestsFromModule(m))
+
+# Run the test suite. 
+runner = unittest.TextTestRunner()
+runner.run(testsuite)
