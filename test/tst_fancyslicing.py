0ec3b7fcde70eec878771b3c44e075b8b64618e5
Jeff Whitaker <jswhit@fastmail.fm>
2009-02-16 19:21:20 +0000

    add some tests for fancy-indexing in __setitem__

--- test/tst_fancyslicing.py
+++ test/tst_fancyslicing.py
@@ -2,26 +2,52 @@ from netCDF4 import Dataset
 from numpy.random import seed, randint
 from numpy.testing import assert_array_equal
 import tempfile, unittest, os, random
-import numpy as NP
+import numpy as np
 
 file_name = tempfile.mktemp(".nc")
 xdim=9; ydim=10; zdim=11
-i = NP.array([2,5,7],'i4')
-ib = NP.array(ydim*[False])
+i = np.array([2,5,7],'i4')
+ib = np.zeros(ydim,dtype=np.bool)
 ib[2] = True; ib[5] = True; ib[7] = True
+ib2 = np.zeros(xdim, dtype=np.bool)
+ib2[1] = True; ib2[4] = True; ib2[6] = True
+# this one should be converted to a slice.
+ib3 = np.zeros(xdim, dtype=np.bool)
+ib3[0] = True; ib2[4] = True; ib2[8] = True
 #seed(9) # fix seed
-data = randint(0,10,size=(xdim,ydim,zdim)).astype('u1')
+data = randint(0,10,size=(xdim,ydim,zdim)).astype('i2')
+data1 = data[:,0,0].copy()
 
 class VariablesTestCase(unittest.TestCase):
 
     def setUp(self):
         self.file = file_name
         f = Dataset(file_name,'w')
-        f.createDimension('x',xdim)
+        f.createDimension('x',None)
         f.createDimension('y',ydim)
         f.createDimension('z',zdim)
         v = f.createVariable('data','i2',('x','y','z'))
-        v[:] = data
+        v1 = f.createVariable('data1','i2','x')
+        self.data1 = data1
+        self.data = data
+        # test __setitem___
+        v[0:xdim] = self.data
+        # integer array slice.
+        v[:,i,:] = -100
+        self.data[:,i,:] = -100
+        # boolen array slice.
+        v[ib2] = -200
+        self.data[ib2] = -200
+        v[ib3,:,:] = -300
+        self.data[ib3,:,:] = -300
+        # same as above, for 1d array
+        v1[0:xdim] = self.data1
+        v1[i] = -100
+        self.data1[i] = -100
+        v1[ib2] = -200
+        self.data1[ib2] = -200
+        v1[ib3] = -300
+        self.data1[ib3] = -300
         f.close()
 
     def tearDown(self):
@@ -33,29 +59,35 @@ class VariablesTestCase(unittest.TestCase):
         f  = Dataset(self.file, 'r')
         v = f.variables['data']
         # slice with an array of integers.
-        assert_array_equal(v[0:-1:2,i,:],data[0:-1:2,i,:])
+        assert_array_equal(v[0:-1:2,i,:],self.data[0:-1:2,i,:])
         # slice with an array of booleans.
-        assert_array_equal(v[0:-1:2,ib,:],data[0:-1:2,ib,:])
+        assert_array_equal(v[0:-1:2,ib,:],self.data[0:-1:2,ib,:])
         # Two slices
-        assert_array_equal(v[1:2,1:3,:], data[1:2,1:3,:])
+        assert_array_equal(v[1:2,1:3,:], self.data[1:2,1:3,:])
         # Three sequences
-        assert_array_equal(v[i,i,i], data[i,i,i])
+        assert_array_equal(v[i,i,i], self.data[i,i,i])
     
         # Two booleans and one slice.  Different from NumPy
-        ibx = NP.array([True, False, True, False, True, False, True, False, True])
-        iby = NP.array([True, False, True, False, True, False, True, False, True, False])
-        ibz = NP.array([True, False, True, False, True, False, True, False, True, False, True])
-        assert_array_equal(v[ibx, iby, :], data[::2, ::2,:])
+        # ibx,ibz should be converted to slice, iby not.
+        ibx = np.array([True, False, True, False, True, False, True, False, True])
+        iby = np.array([True, False, True, False, False, False, True, False, True, False])
+        ibz = np.array([True, False, True, False, True, False, True, False,\
+            True, False, True])
+        datatmp = self.data[::2,:,:]
+        datatmp = datatmp[:,iby,:]
+        assert_array_equal(v[ibx, iby, :], datatmp)
         
         # Three booleans
-        assert_array_equal(v[ibx,iby,ibz], data[::2, ::2, ::2])
+        datatmp = self.data[::2,:,:]
+        datatmp = datatmp[:,iby,::2]
+        assert_array_equal(v[ibx,iby,ibz], datatmp)
         
         # Ellipse
-        assert_array_equal(v[...,::2],data[..., ::2])
-        assert_array_equal(v[...,::-2],data[..., ::-2])
-        assert_array_equal(v[[1,2],...],data[[1,2],...])
+        assert_array_equal(v[...,::2],self.data[..., ::2])
+        assert_array_equal(v[...,::-2],self.data[..., ::-2])
+        assert_array_equal(v[[1,2],...],self.data[[1,2],...])
         
-        assert_array_equal(v[0], data[0])
+        assert_array_equal(v[0], self.data[0])
 
 if __name__ == '__main__':
     unittest.main()
