9e256b4ce239da124410d1ebafc2d76477cc2076
Jeff Whitaker <jswhit@fastmail.fm>
2009-03-31 17:18:06 +0000

    temporary mod to work around a bug in netcdf 4.0.1

--- test/tst_dap.py
+++ test/tst_dap.py
@@ -3,10 +3,17 @@ import netCDF4
 
 # test accessing data over http with opendap.
 
-URL = 'http://test.opendap.org/dap/data/nc/test.nc'
-firstvarname = 'b44'
-firstvarmin = -30
-firstvarmax = -12
+# due to a bug in netcdf 4.0.1, this URL will fail.
+#URL = 'http://test.opendap.org/dap/data/nc/test.nc'
+#firstvarname = 'b44'
+#firstvarmin = -30
+#firstvarmax = -12
+#firstvarshape = (4,4)
+URL = 'http://test.opendap.org/dap/data/nc/testfile.nc'
+firstvarname = 'aa'
+firstvarmin = -2
+firstvarmax = -0
+firstvarshape = (4,)
 
 class DapTestCase(unittest.TestCase):
 
@@ -21,7 +28,7 @@ class DapTestCase(unittest.TestCase):
         file = netCDF4.Dataset(URL)
         assert file.variables.keys()[0] == firstvarname
         firstvar = file.variables[firstvarname]
-        assert firstvar.shape == (4,4)
+        assert firstvar.shape == firstvarshape
         data = firstvar[:]
         assert data.min() == firstvarmin
         assert data.max() == firstvarmax
