b0cc0ed538a2882dc6e286a4fca1f453929511d5
Jeff Whitaker <jswhit@fastmail.fm>
2009-10-21 15:58:16 +0000

    added support for szip compression.

---
+++ examples/bench_compress.py
@@ -0,0 +1,48 @@
+# benchmark reads and writes, with and without compression.
+# tests all four supported file formats.
+from numpy.random.mtrand import uniform
+import netCDF4
+from timeit import Timer
+import os
+
+# create an n1dim by n2dim by n3dim random array.
+n1dim = 30   
+n2dim = 15
+n3dim = 73
+n4dim = 144
+ntrials = 10
+print 'reading and writing a %s by %s by %s by %s random array ..'%(n1dim,n2dim,n3dim,n4dim)
+print '(average of %s trials)' % ntrials
+array = netCDF4._quantize(uniform(size=(n1dim,n2dim,n3dim,n4dim)),4)
+
+
+def write_netcdf(filename,zlib=False,shuffle=False,szip=False,szip_encoding=False,szip_bits_per_block=32):
+    file = netCDF4.Dataset(filename,'w',format='NETCDF4')
+    file.createDimension('n1', n1dim)
+    file.createDimension('n2', n2dim)
+    file.createDimension('n3', n3dim)
+    file.createDimension('n4', n4dim)
+    foo = file.createVariable('data',\
+            'f8',('n1','n2','n3','n4'),zlib=zlib,shuffle=shuffle,\
+            szip=szip,szip_encoding=szip_encoding,szip_bits_per_block=szip_bits_per_block)
+    foo[:] = array
+    file.close()
+
+def read_netcdf(filename):
+    file = netCDF4.Dataset(filename)
+    data = file.variables['data'][:]
+    file.close()
+
+for compress_kwargs in ["zlib=False,szip=False","zlib=True,shuffle=False",
+                        "zlib=True,shuffle=True",
+                        "szip=True,szip_encoding='nn',szip_bits_per_block=16",\
+                        "szip=True,szip_encoding='ec',szip_bits_per_block=16"]:
+    print 'testing compression ...',compress_kwargs
+    # writing.
+    t = Timer("write_netcdf('test.nc',%s)" % compress_kwargs,"from __main__ import write_netcdf")
+    print 'writing took',sum(t.repeat(ntrials,1))/ntrials,'seconds'
+    # test reading.
+    t = Timer("read_netcdf('test.nc')","from __main__ import read_netcdf")
+    print 'reading took',sum(t.repeat(ntrials,1))/ntrials,'seconds'
+    # print out size of resulting files.
+    print 'size of test.nc = ',os.stat('test.nc').st_size
