a75e2133e7b35ff4bf88b9064b3943e3211caa9d
Jeff Whitaker <jswhit@fastmail.fm>
2010-01-22 23:25:53 +0000

    add comment

--- pygrib/test.py
+++ pygrib/test.py
@@ -1,14 +1,15 @@
 import pygrib
 import sys
 import numpy as np
+# lists information for grib file specified on command line.
 filename = sys.argv[1]
 n = 0
 for grb in pygrib.open(filename):
     n = n + 1
-    data = grb['values']
     print '------message %d------' %n
     for k in grb.keys():
         if k.startswith('mars'): continue
         if k == 'values' or k == 'codedValues': continue
         print k,'=',grb[k]
+    data = grb['values']
     print 'min/max of data = ',data.min(),data.max()
