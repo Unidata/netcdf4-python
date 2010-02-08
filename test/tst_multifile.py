c699db93de51378a673b0562d8b851ad77dcd8b9
Jeff Whitaker <jswhit@fastmail.fm>
2010-02-08 12:52:37 +0000

    add testing for masked MFDataset vars.

--- test/tst_multifile.py
+++ test/tst_multifile.py
@@ -2,6 +2,7 @@ from netCDF4 import Dataset, MFDataset
 import numpy
 from numpy.random import seed, randint
 from numpy.testing import assert_array_equal
+from numpy import ma
 import tempfile, unittest, os
 
 nx=100; ydim=5; zdim=1
@@ -9,6 +10,9 @@ nfiles = 10
 ninc = nx/nfiles
 files = [tempfile.mktemp(".nc") for nfile in range(nfiles)]
 data = randint(0,10,size=(nx,ydim,zdim))
+missval = 99
+data[::10] = missval
+data = ma.masked_values(data,missval)
 
 class VariablesTestCase(unittest.TestCase):
 
@@ -24,6 +28,7 @@ class VariablesTestCase(unittest.TestCase):
             x.units = 'zlotys'
             dat = f.createVariable('data','i',('x','y','z',))
             dat.name = 'phony data' 
+            dat.missing_value = missval
             nx1 = nfile*ninc; nx2 = ninc*(nfile+1)
             #x[0:ninc] = numpy.arange(nfile*ninc,ninc*(nfile+1))
             x[:] = numpy.arange(nfile*ninc,ninc*(nfile+1))
@@ -42,12 +47,16 @@ class VariablesTestCase(unittest.TestCase):
         assert f.history == 'created today'
         assert_array_equal(numpy.arange(0,nx),f.variables['x'][:])
         varin = f.variables['data']
+        datin = varin[:]
+        assert_array_equal(datin.mask,data.mask)
+        varin.set_auto_maskandscale(False)
+        data2 = data.filled()
         assert varin.name == 'phony data'
         assert varin.shape == (nx,ydim,zdim)
         assert varin.dimensions == ('x','y','z')
-        assert_array_equal(varin[4:-4:4,3:5,2:8],data[4:-4:4,3:5,2:8])
-        assert varin[0,0,0] == data[0,0,0]
-        assert_array_equal(varin[:],data)
+        assert_array_equal(varin[4:-4:4,3:5,2:8],data2[4:-4:4,3:5,2:8])
+        assert varin[0,0,0] == data2[0,0,0]
+        assert_array_equal(varin[:],data2)
         assert getattr(varin,'nonexistantatt',None) == None
         f.close()
 
