e6cc15bf3b583f2b65f6522a131449f871d2d7f7
Jeff Whitaker <jswhit@fastmail.fm>
2010-09-20 15:33:31 +0000

    prettier output

--- checkversion.py
+++ checkversion.py
@@ -1,4 +1,4 @@
 import netCDF4
-print netCDF4.__version__
-print netCDF4.__hdf5libversion__
-print netCDF4.__netcdf4libversion__
+print 'netcdf4-python version:',netCDF4.__version__
+print 'HDF5 lib version:      ',netCDF4.__hdf5libversion__
+print 'netcdflib version:     ',netCDF4.__netcdf4libversion__
