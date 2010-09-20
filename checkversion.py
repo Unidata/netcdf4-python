71dddef6a1b99836f5759ee745d64cc67664071b
Jeff Whitaker <jswhit@fastmail.fm>
2010-09-20 15:33:46 +0000

    update

--- checkversion.py
+++ checkversion.py
@@ -1,4 +1,4 @@
 import netCDF4
 print 'netcdf4-python version:',netCDF4.__version__
 print 'HDF5 lib version:      ',netCDF4.__hdf5libversion__
-print 'netcdflib version:     ',netCDF4.__netcdf4libversion__
+print 'netcdf lib version:    ',netCDF4.__netcdf4libversion__
