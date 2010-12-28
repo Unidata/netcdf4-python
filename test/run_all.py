b2ecb2f42dfd0d94e9ad754c9c457c37c7011862
Jeff Whitaker <jswhit@fastmail.fm>
2010-12-28 17:04:30 +0000

    remove unused code

--- test/run_all.py
+++ test/run_all.py
@@ -15,16 +15,8 @@ os.environ['PYTHONPATH'] = py_path
 testsuite = unittest.TestSuite()
 version = getlibversion().split()[0]
 for f in test_files:
-    ff = os.path.join(sys.path[0],f)
     m = __import__(os.path.splitext(f)[0])
-    if m.__name__ == 'tst_compoundvar':
-        if version < "4.1":
-            print \
-            "skipping tst_compoundvar, requires netcdf 4.1 or higher, you"\
-            +" have %s." % version
-            continue
     testsuite.addTests(unittest.TestLoader().loadTestsFromModule(m))
-    
 
 # Run the test suite. 
 
@@ -34,4 +26,4 @@ def test(verbosity=1):
     runner.run(testsuite)
 
 if __name__ == '__main__':
-    test()
+    test(verbosity=1)
