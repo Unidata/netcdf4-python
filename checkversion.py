cf71ec8df7bbb467852c8597521e32d5413de2bb
Jeff Whitaker <jswhit@fastmail.fm>
2011-03-01 19:16:52 +0000

    use sys.stdout.write instead of print

--- checkversion.py
+++ checkversion.py
@@ -1,4 +1,5 @@
 import netCDF4
-print 'netcdf4-python version:',netCDF4.__version__
-print 'HDF5 lib version:      ',netCDF4.__hdf5libversion__
-print 'netcdf lib version:    ',netCDF4.__netcdf4libversion__
+import sys
+sys.stdout.write('netcdf4-python version: %s\n'%netCDF4.__version__)
+sys.stdout.write('HDF5 lib version:       %s\n'%netCDF4.__hdf5libversion__)
+sys.stdout.write('netcdf lib version:     %s\n'%netCDF4.__netcdf4libversion__)
