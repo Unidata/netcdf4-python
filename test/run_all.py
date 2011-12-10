67f1e6e41e0a6207797ab34acd1e3a0ae6f6a452
Jeff Whitaker <jswhit@fastmail.fm>
2011-12-10 15:37:01 +0000

    add unicode tests.

--- test/run_all.py
+++ test/run_all.py
@@ -1,9 +1,15 @@
 import glob, os, sys, unittest, netCDF4
 from netCDF4 import getlibversion,__hdf5libversion__,__netcdf4libversion__,__version__
 
+python3 = sys.version_info[0] > 2
+
 __all__ = ['test']
 # Find all test files.
 test_files = glob.glob('tst_*.py')
+if python3:
+    test_files.remove('tst_unicode.py')
+else:
+    test_files.remove('tst_unicode3.py')
 py_path = os.environ.get('PYTHONPATH')
 if py_path is None:
     py_path = '.'
@@ -27,7 +33,6 @@ def test(verbosity=1):
 
 if __name__ == '__main__':
     test(verbosity=1)
-    import sys
     sys.stdout.write('\n')
     sys.stdout.write('netcdf4-python version: %s\n' % __version__)
     sys.stdout.write('HDF5 lib version:       %s\n' % __hdf5libversion__)
---
+++ test/tst_unicode3.py
@@ -0,0 +1,42 @@
+import netCDF4
+import numpy as np
+import sys, unittest, os, tempfile
+
+netCDF4.default_encoding = 'utf-8'
+
+FILE_NAME = tempfile.mktemp(".nc")
+ATT1 = '\u03a0\u03a3\u03a9'
+ATT2 = 'x\xb0'
+ATT3 = ['\u03a0','\u03a3','\u03a9']
+DIM_NAME = 'x\xb0'
+VAR_NAME = 'Andr\xe9'
+
+class UnicodeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.file = FILE_NAME
+        f = netCDF4.Dataset(self.file,'w')
+        f.attribute1 = ATT1
+        f.attribute2 = ATT2
+        f.attribute3 = ATT3
+        d = f.createDimension(DIM_NAME, None)
+        v = f.createVariable(VAR_NAME, np.float, (DIM_NAME,))
+        f.close()
+
+    def tearDown(self):
+        # Remove the temporary files
+        os.remove(self.file)
+
+    def runTest(self):
+        """testing unicode"""
+        f  = netCDF4.Dataset(self.file, 'r')
+        d = f.dimensions[DIM_NAME]
+        v = f.variables[VAR_NAME]
+        # check accessing individual attributes.
+        assert f.attribute1 == ATT1
+        assert f.attribute2 == ATT2
+        assert f.attribute3 == ''.join(ATT3)
+        f.close()
+
+if __name__ == '__main__':
+    unittest.main()
