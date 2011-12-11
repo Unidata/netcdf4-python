4eb4a5395a7682dc160f20ba1eccf9e40c2845dc
Jeff Whitaker <jswhit@fastmail.fm>
2011-12-11 00:08:32 +0000

    fix for python 3

--- examples/tutorial.py
+++ examples/tutorial.py
@@ -4,7 +4,7 @@ from netCDF4 import Dataset
 
 # create a file (Dataset object, also the root group).
 rootgrp = Dataset('test.nc', 'w', format='NETCDF4')
-print rootgrp.file_format
+print(rootgrp.file_format)
 rootgrp.close()
 
 # create some groups.
@@ -21,10 +21,10 @@ def walktree(top):
     for value in top.groups.values():
         for children in walktree(value):
             yield  children
-print rootgrp
+print(rootgrp)
 for children in walktree(rootgrp):
      for child in children:
-         print child
+         print(child)
 
 # dimensions.
 level = rootgrp.createDimension('level', None)
@@ -32,16 +32,16 @@ time = rootgrp.createDimension('time', None)
 lat = rootgrp.createDimension('lat', 73)
 lon = rootgrp.createDimension('lon', 144)
 
-print rootgrp.dimensions
+print(rootgrp.dimensions)
 
-print len(lon)
-print lon.isunlimited()
-print time.isunlimited()
+print(len(lon))
+print(lon.isunlimited())
+print(time.isunlimited())
 
 for dimobj in rootgrp.dimensions.values():
-    print dimobj
+    print(dimobj)
 
-print time
+print(time)
 
 # variables.
 times = rootgrp.createVariable('time','f8',('time',))
@@ -67,15 +67,15 @@ times.units = 'hours since 0001-01-01 00:00:00.0'
 times.calendar = 'gregorian'
 
 for name in rootgrp.ncattrs():
-    print 'Global attr', name, '=', getattr(rootgrp,name)
+    print('Global attr', name, '=', getattr(rootgrp,name))
 
-print rootgrp
+print(rootgrp)
 
-print rootgrp.__dict__
+print(rootgrp.__dict__)
 
-print rootgrp.variables
+print(rootgrp.variables)
 
-print rootgrp.variables['temp']
+print(rootgrp.variables['temp'])
 
 import numpy
 # no unlimited dimension, just assign to slice.
@@ -83,34 +83,34 @@ lats =  numpy.arange(-90,91,2.5)
 lons =  numpy.arange(-180,180,2.5)
 latitudes[:] = lats
 longitudes[:] = lons
-print 'latitudes =\n',latitudes[:]
-print 'longitudes =\n',longitudes[:]
+print('latitudes =\n',latitudes[:])
+print('longitudes =\n',longitudes[:])
 
 # append along two unlimited dimensions by assigning to slice.
 nlats = len(rootgrp.dimensions['lat'])
 nlons = len(rootgrp.dimensions['lon'])
-print 'temp shape before adding data = ',temp.shape
+print('temp shape before adding data = ',temp.shape)
 from numpy.random.mtrand import uniform # random number generator.
 temp[0:5,0:10,:,:] = uniform(size=(5,10,nlats,nlons))
-print 'temp shape after adding data = ',temp.shape
+print('temp shape after adding data = ',temp.shape)
 # levels have grown, but no values yet assigned.
-print 'levels shape after adding pressure data = ',levels.shape
+print('levels shape after adding pressure data = ',levels.shape)
 
 # assign values to levels dimension variable.
 levels[:] =  [1000.,850.,700.,500.,300.,250.,200.,150.,100.,50.]
 # fancy slicing
 tempdat = temp[::2, [1,3,6], lats>0, lons>0]
-print 'shape of fancy temp slice = ',tempdat.shape
-print temp[0, 0, [0,1,2,3], [0,1,2,3]].shape
+print('shape of fancy temp slice = ',tempdat.shape)
+print(temp[0, 0, [0,1,2,3], [0,1,2,3]].shape)
 
 # fill in times.
 from datetime import datetime, timedelta
 from netCDF4 import num2date, date2num, date2index
 dates = [datetime(2001,3,1)+n*timedelta(hours=12) for n in range(temp.shape[0])]
 times[:] = date2num(dates,units=times.units,calendar=times.calendar)
-print 'time values (in units %s): ' % times.units+'\\n',times[:]
+print('time values (in units %s): ' % times.units+'\\n',times[:])
 dates = num2date(times[:],units=times.units,calendar=times.calendar)
-print 'dates corresponding to time values:\\n',dates
+print('dates corresponding to time values:\\n',dates)
 
 rootgrp.close()
 
@@ -125,14 +125,14 @@ for nfile in range(10):
 # now read all those files in at once, in one Dataset.
 from netCDF4 import MFDataset
 f = MFDataset('mftest*nc')
-print f.variables['x'][:]
+print(f.variables['x'][:])
 
 # example showing how to save numpy complex arrays using compound types.
 f = Dataset('complex.nc','w')
 size = 3 # length of 1-d complex array
 # create sample complex data.
 datac = numpy.exp(1j*(1.+numpy.linspace(0, numpy.pi, size)))
-print datac.dtype
+print(datac.dtype)
 # create complex128 compound data type.
 complex128 = numpy.dtype([('real',numpy.float64),('imag',numpy.float64)])
 complex128_t = f.createCompoundType(complex128,'complex128')
@@ -145,20 +145,20 @@ v[:] = data
 # close and reopen the file, check the contents.
 f.close()
 f = Dataset('complex.nc')
-print f
-print f.variables['cmplx_var']
-print f.cmptypes
-print f.cmptypes['complex128']
+print(f)
+print(f.variables['cmplx_var'])
+print(f.cmptypes)
+print(f.cmptypes['complex128'])
 v = f.variables['cmplx_var']
-print v.shape
+print(v.shape)
 datain = v[:] # read in all the data into a numpy structured array
 # create an empty numpy complex array
 datac2 = numpy.empty(datain.shape,numpy.complex128)
 # .. fill it with contents of structured array.
 datac2.real = datain['real']
 datac2.imag = datain['imag']
-print datac.dtype,datac
-print datac2.dtype,datac2
+print(datac.dtype,datac)
+print(datac2.dtype,datac2)
 # more complex compound type example.
 from netCDF4 import chartostring, stringtoarr
 f = Dataset('compound_example.nc','w') # create a new dataset.
@@ -211,7 +211,7 @@ statdat[0] = data
 statdat[1] = (40.78,-73.99,(-12.5,90),
              (290.2,282.5,279.,277.9,276.,266.,264.1,260.,255.5,243.),
              range(900,400,-50),stringtoarr('New York, New York, USA',NUMCHARS))
-print f.cmptypes
+print(f.cmptypes)
 windunits = numpy.empty(1,winddtype_units)
 stationobs_units = numpy.empty(1,statdtype_units)
 windunits['speed'] = stringtoarr('m/s',NUMCHARS)
@@ -226,23 +226,25 @@ statdat.units = stationobs_units
 # close and reopen the file.
 f.close()
 f = Dataset('compound_example.nc')
-print f
+print(f)
 statdat = f.variables['station_obs']
+print(statdat)
 # print out data in variable.
-print 'data in a variable of compound type:'
-print '----'
+print('data in a variable of compound type:')
+print('----')
 for data in statdat[:]:
     for name in statdat.dtype.names:
         if data[name].dtype.kind == 'S': # a string
             # convert array of characters back to a string for display.
-            print name,': value =',chartostring(data[name]),\
-            ': units=',chartostring(statdat.units[name])
+            print(name,': value =',chartostring(data[name]),\
+            ': units=',chartostring(statdat.units[name]))
         elif data[name].dtype.kind == 'V': # a nested compound type
-            print name,data[name].dtype.names,': value=',data[name],': units=',\
-            tuple([chartostring(s) for s in tuple(statdat.units[name])])
+            print(name,data[name].dtype.names,': value=',data[name],': units=',\
+            tuple([chartostring(s) for s in tuple(statdat.units[name])]))
         else: # a numeric type.
-            print name,': value=',data[name],': units=',chartostring(statdat.units[name])
-    print '----'
+            print(name,': value=',data[name],': units=',\
+                  chartostring(statdat.units[name]))
+    print('----')
 f.close()
 
 f = Dataset('tst_vlen.nc','w')
@@ -256,10 +258,11 @@ for n in range(len(y)*len(x)):
     data[n] = numpy.arange(random.randint(1,10),dtype='int32')+1
 data = numpy.reshape(data,(len(y),len(x)))
 vlvar[:] = data
-print 'vlen variable =\n',vlvar[:]
-print f
-print f.variables['phony_vlen_var']
-print f.vltypes['phony_vlen']
+print(vlvar)
+print('vlen variable =\n',vlvar[:])
+print(f)
+print(f.variables['phony_vlen_var'])
+print(f.vltypes['phony_vlen'])
 z = f.createDimension('z', 10)
 strvar = f.createVariable('strvar',str,'z')
 chars = '1234567890aabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -268,7 +271,7 @@ for n in range(10):
     stringlen = random.randint(2,12)
     data[n] = ''.join([random.choice(chars) for i in range(stringlen)])
 strvar[:] = data
-print 'variable-length string variable:\n',strvar[:]
-print f
-print f.variables['strvar']
+print('variable-length string variable:\n',strvar[:])
+print(f)
+print(f.variables['strvar'])
 f.close()
