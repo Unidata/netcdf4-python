d98f6289815780d25cfdf5f044ba687c6fd1b0cf
Jeff Whitaker <jswhit@fastmail.fm>
2012-03-27 19:40:31 +0000

    diskless file test.

---
+++ test/tst_diskless.py
@@ -0,0 +1,45 @@
+import unittest, os, tempfile
+import numpy as np
+from numpy.random.mtrand import uniform 
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+import netCDF4
+
+# rudimentary test of diskless file capability.
+
+# create an n1dim by n2dim by n3dim random array
+n1dim = 10
+n2dim = 73 
+n3dim = 144
+ranarr = 100.*uniform(size=(n1dim,n2dim,n3dim))
+FILE_NAME = tempfile.mktemp(".nc")
+
+class DisklessTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.file = FILE_NAME
+        f = netCDF4.Dataset(self.file,'w',diskless=True)
+        self.f = f
+        # foo has a single unlimited dimension
+        f.createDimension('n1', n1dim)
+        f.createDimension('n2', n2dim)
+        f.createDimension('n3', n3dim)
+        foo = f.createVariable('data1', ranarr.dtype.str[1:], ('n1','n2','n3'))
+        # write some data to it.
+        foo[0:n1dim-1] = ranarr[:-1,:,:]
+        foo[n1dim-1] = ranarr[-1,:,:]
+
+    def tearDown(self):
+        self.f.close()
+
+    def runTest(self):
+        """testing diskless file capability"""
+        foo = self.f.variables['data1']
+        # check shape.
+        self.assert_(foo.shape == (n1dim,n2dim,n3dim))
+        # check data.
+        assert_array_almost_equal(foo[:], ranarr)
+        # file does not actually exist on disk
+        assert(os.path.isfile(self.file)==False)
+
+if __name__ == '__main__':
+    unittest.main()
