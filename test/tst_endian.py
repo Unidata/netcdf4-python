a1b8c56e14370036d31a49c31751ea3436937125
Jeff Whitaker <jswhit@fastmail.fm>
2013-09-25 14:26:37 +0000

    update

--- test/tst_endian.py
+++ test/tst_endian.py
@@ -4,30 +4,44 @@ import unittest, os, tempfile
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 data = np.arange(12,dtype='f4').reshape(3,4)
-little = data.astype('<f4')
-big = data.astype('>f4')
 FILE_NAME = tempfile.mktemp(".nc")
 FILE_NAME2 = tempfile.mktemp(".nc")
+
+def create_file(file,format,data):
+    dataset = netCDF4.Dataset(file,'w',format=format)
+    dataset.createDimension('time', None)
+    dataset.createDimension('space', 4)
+    dims = ('time', 'space')
+    little = data.astype('<f4')
+    big = data.astype('>f4')
+    ll = dataset.createVariable('little-little', '<f4', dims)
+    lb = dataset.createVariable('little-big', '<f4', dims)
+    bl = dataset.createVariable('big-little', '>f4', dims)
+    bb = dataset.createVariable('big-big', '>f4', dims)
+    ll[:] = little
+    lb[:] = big
+    bl[:] = little
+    bb[:] = big
+    dataset.close()
+
+def check_data(file, data):
+    f = netCDF4.Dataset(file)
+    ll = f.variables['little-little'][:]
+    lb = f.variables['little-big'][:]
+    bb = f.variables['big-big'][:]
+    bl = f.variables['big-little'][:]
+    # check data.
+    assert_array_almost_equal(ll, data)
+    assert_array_almost_equal(lb, data)
+    assert_array_almost_equal(bl, data)
+    assert_array_almost_equal(bb, data)
+    f.close()
 
 class EndianTestCase(unittest.TestCase):
 
     def setUp(self):
-        def create_file(file,format):
-            dataset = netCDF4.Dataset(file,'w',format=format)
-            dataset.createDimension('time', None)
-            dataset.createDimension('space', 4)
-            dims = ('time', 'space')
-            ll = dataset.createVariable('little-little', '<f4', dims)
-            lb = dataset.createVariable('little-big', '<f4', dims)
-            bl = dataset.createVariable('big-little', '>f4', dims)
-            bb = dataset.createVariable('big-big', '>f4', dims)
-            ll[:] = little
-            lb[:] = big
-            bl[:] = little
-            bb[:] = big
-            dataset.close()
-        create_file(FILE_NAME,'NETCDF4_CLASSIC'); self.file=FILE_NAME
-        create_file(FILE_NAME2,'NETCDF3_CLASSIC'); self.file2=FILE_NAME2
+        create_file(FILE_NAME,'NETCDF4_CLASSIC',data); self.file=FILE_NAME
+        create_file(FILE_NAME2,'NETCDF3_CLASSIC',data); self.file2=FILE_NAME2
 
     def tearDown(self):
         # Remove the temporary files
@@ -36,20 +50,8 @@ class EndianTestCase(unittest.TestCase):
 
     def runTest(self):
         """testing endian conversion capability"""
-        def check_data(file):
-            f = netCDF4.Dataset(file)
-            ll = f.variables['little-little']
-            lb = f.variables['little-big']
-            bb = f.variables['big-big']
-            bl = f.variables['big-little']
-            # check data.
-            assert_array_almost_equal(ll[:], data)
-            assert_array_almost_equal(lb[:], data)
-            assert_array_almost_equal(bl[:], data)
-            assert_array_almost_equal(bb[:], data)
-            f.close()
-        check_data(self.file)
-        check_data(self.file2)
+        check_data(self.file, data)
+        check_data(self.file2, data)
 
 if __name__ == '__main__':
     unittest.main()
