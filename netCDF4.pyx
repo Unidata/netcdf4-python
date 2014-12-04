a93a93aded98c66e90f60a3287501f3ef0856ed8
Jeff Whitaker <jswhit@fastmail.fm>
2014-12-04 12:17:43 -0700

    fix typo in docs

--- netCDF4.pyx
+++ netCDF4.pyx
@@ -463,7 +463,7 @@ with C{[0, array([True, False, True]), array([False, True, True, True]), :]}
 would return a C{(2, 3, 5)} array. In NumPy, this would raise an error since
 it would be equivalent to C{[0, [0,1], [1,2,3], :]}. When slicing with integer
 sequences, the indices must be sorted in increasing order and contain no duplicates.
-While this behaviour may some confusion for those used to NumPy's 'fancy indexing' rules,
+While this behaviour may cause some confusion for those used to NumPy's 'fancy indexing' rules,
 it provides a very powerful way to extract data from multidimensional netCDF
 variables by using logical operations on the dimension arrays to create slices.
 
