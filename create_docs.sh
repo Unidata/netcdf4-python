7dd75f0290eaa0fa66f054155626b1399adf2fe9
Jeff Whitaker <jswhit@fastmail.fm>
2015-05-17 10:36:11 -0600

    branch to work on transition of docs from epydoc to pdoc (using markdown)

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,1 @@
-# svn propset svn:mime-type text/html docs/*html
-epydoc -v --no-frames --no-private --introspect-only --name netcdf4-python -o docs netCDF4._netCDF4
+pdoc --html --overwrite --html-dir 'docs' netCDF4
--- netCDF4/__init__.py
+++ netCDF4/__init__.py
@@ -6,3 +6,5 @@ from ._netCDF4 import __doc__
 from ._netCDF4 import (__version__, __netcdf4libversion__, __hdf5libversion__,
                        __has_rename_grp__, __has_nc_inq_path__,
                        __has_nc_inq_format_extended__)
+__all__ =\
+['Dataset','Variable','Dimension','Group','MFDataset','MFTime','CompoundType','VLType','date2num','num2date','date2index','stringtochar','chartostring','stringtoarr','getlibversion']
