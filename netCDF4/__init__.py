02cd33310462f104fdc5af90c06fe42e4abfc659
Jeff Whitaker <jswhit@fastmail.fm>
2015-05-18 08:05:06 -0600

    include __pdoc__ to document instance variables.

--- netCDF4/__init__.py
+++ netCDF4/__init__.py
@@ -8,3 +8,9 @@ from ._netCDF4 import (__version__, __netcdf4libversion__, __hdf5libversion__,
                        __has_nc_inq_format_extended__)
 __all__ =\
 ['Dataset','Variable','Dimension','Group','MFDataset','MFTime','CompoundType','VLType','date2num','num2date','date2index','stringtochar','chartostring','stringtoarr','getlibversion']
+__pdoc__={}
+__pdoc__['Dataset.path'] = \
+"""The `path` attribute shows the location of the `netCDF4.Group` in
+the `netCDF4.Dataset` in a unix directory format (the names of groups in the
+hierarchy separated by backslashes). A `netCDF4.Dataset` instance is the root
+group, so the path is simply `'/'`."""
