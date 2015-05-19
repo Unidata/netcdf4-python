b0b4d13bddd0025b1b70f033aa0e89bc4a589fd4
Jeff Whitaker <jswhit@fastmail.fm>
2015-05-19 11:21:55 -0600

    update

--- create_docs.sh
+++ create_docs.sh
@@ -1,1 +1,4 @@
+# Note: pdoc.path needs to be applied to pdoc source
+# or else cython extension method docstrings will
+# not be extracted.
 pdoc --html --html-no-source --overwrite --html-dir 'docs' netCDF4
