64ebe9e6174a1340030d64908b1437549fa93e9a
Jeff Whitaker <jswhit@fastmail.fm>
2015-05-19 11:22:58 -0600

    update

--- create_docs.sh
+++ create_docs.sh
@@ -1,3 +1,5 @@
+# Uses pdoc (https://github.com/BurntSushi/pdoc)
+# to create html docs from docstrings in Cython source.
 # Note: pdoc.path needs to be applied to pdoc source
 # or else cython extension method docstrings will
 # not be extracted.
