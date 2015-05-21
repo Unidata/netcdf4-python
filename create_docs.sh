d72d73ec76b89051aa925540cb4eb330d7fcb1aa
Jeff Whitaker <jswhit@fastmail.fm>
2015-05-21 16:18:51 -0600

    remove un-needed file

--- create_docs.sh
+++ create_docs.sh
@@ -1,6 +1,6 @@
 # Uses pdoc (https://github.com/BurntSushi/pdoc)
 # to create html docs from docstrings in Cython source.
-# Note: pdoc.path needs to be applied to pdoc source
-# or else cython extension method docstrings will
-# not be extracted.
+# Use hacked version at https://github.com/jswhit/pdoc
+# which extracts cython method docstrings and function signatures.
 pdoc --html --html-no-source --overwrite --html-dir 'docs' netCDF4
+pdoc netCDF4 >! docs.md
