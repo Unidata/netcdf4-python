f335160d76fa69d1365281700182aa37695a17fe
Jeff Whitaker <jswhit@fastmail.fm>
2015-09-21 09:17:42 -0600

    remove un-needed files

--- checkversion.py
+++
@@ -1,5 +0,0 @@
-import netCDF4
-import sys
-sys.stdout.write('netcdf4-python version: %s\n'%netCDF4.__version__)
-sys.stdout.write('HDF5 lib version:       %s\n'%netCDF4.__hdf5libversion__)
-sys.stdout.write('netcdf lib version:     %s\n'%netCDF4.__netcdf4libversion__)
--- create_docs.sh
+++
@@ -1,2 +0,0 @@
-# svn propset svn:mime-type text/html docs/*html
-epydoc -v --no-frames --no-private --introspect-only -o docs netCDF4 
