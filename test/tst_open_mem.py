0a32144128d80d5483f61ac6d6818fc92ada7fbe
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-04-29 18:43:16 -0700

    only run test on versions which have fix for nc_open_mem

--- test/tst_open_mem.py
+++ test/tst_open_mem.py
@@ -7,15 +7,17 @@ CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class TestOpenMem(unittest.TestCase):
     def test_mem_open(self):
-        fpath = os.path.join(CURRENT_DIR, "netcdf_dummy_file.nc")
+        # Needs: https://github.com/Unidata/netcdf-c/pull/400
+        if netCDF4.__netcdf4libversion__ >= '4.4.2':
+            fpath = os.path.join(CURRENT_DIR, "netcdf_dummy_file.nc")
 
-        with open(fpath, 'rb') as f:
-            nc_bytes = f.read()
+            with open(fpath, 'rb') as f:
+                nc_bytes = f.read()
 
-        with netCDF4.Dataset('foo_bar', memory=nc_bytes) as nc:
-            print(nc.filepath())
-            assert nc.filepath() == 'foo_bar'
-            assert nc.project_summary == 'Dummy netCDF file'
+            with netCDF4.Dataset('foo_bar', memory=nc_bytes) as nc:
+                print(nc.filepath())
+                assert nc.filepath() == 'foo_bar'
+                assert nc.project_summary == 'Dummy netCDF file'
 
 if __name__ == '__main__':
     unittest.main()
