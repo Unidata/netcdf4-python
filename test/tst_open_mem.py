ab203860ea8d417be91ec92124bbb441037f8fc2
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-04-29 18:44:23 -0700

    update ver

--- test/tst_open_mem.py
+++ test/tst_open_mem.py
@@ -8,7 +8,7 @@ CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 class TestOpenMem(unittest.TestCase):
     def test_mem_open(self):
         # Needs: https://github.com/Unidata/netcdf-c/pull/400
-        if netCDF4.__netcdf4libversion__ >= '4.4.2':
+        if netCDF4.__netcdf4libversion__ >= '4.4.1.2':
             fpath = os.path.join(CURRENT_DIR, "netcdf_dummy_file.nc")
 
             with open(fpath, 'rb') as f:
