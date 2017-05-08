dd8aa3759b3020983ff84c12563a908dcd0f06b8
Jeff Whitaker <jswhit@fastmail.fm>
2017-05-08 13:16:09 -0600

    change version number to 1.2.8

--- netCDF4/_netCDF4.pyx
+++ netCDF4/_netCDF4.pyx
@@ -1,5 +1,5 @@
 """
-Version 1.2.8a0
+Version 1.2.8
 -------------
 - - - 
 
@@ -936,7 +936,7 @@ except ImportError:
     # python3: zip is already python2's itertools.izip
     pass
 
-__version__ = "1.2.8a0"
+__version__ = "1.2.8"
 
 # Initialize numpy
 import posixpath
