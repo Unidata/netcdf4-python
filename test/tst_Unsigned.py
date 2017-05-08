e6b56c0fc98cc2623b25dda82d7d819e41af84e1
Jeff Whitaker <jswhit@fastmail.fm>
2017-05-08 13:18:02 -0600

    add test data file

--- test/tst_Unsigned.py
+++ test/tst_Unsigned.py
@@ -1,15 +1,11 @@
-import os
 import unittest
 import netCDF4
 from numpy.testing import assert_array_equal
 import numpy as np
 
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-
 class Test_Unsigned(unittest.TestCase):
     def test_unsigned(self):
-        fpath = os.path.join(CURRENT_DIR, "ubyte.nc3")
-        f = netCDF4.Dataset(fpath)
+        f = netCDF4.Dataset("ubyte.nc3")
         data = f['ub'][:]
         assert data.dtype.str[1:] == 'u1'
         assert_array_equal(data,np.array([0,255],np.uint8))
