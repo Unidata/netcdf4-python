74b0353205be7ca0e3f2a41a754f869b5eeffbaf
Jeff Whitaker <jswhit@fastmail.fm>
2017-05-17 10:18:51 -0600

    add test for _Encoding with char arrays

--- test/tst_stringarr.py
+++ test/tst_stringarr.py
@@ -1,20 +1,22 @@
 from netCDF4 import Dataset, stringtochar, chartostring
-import random, numpy
+import random, numpy, string
 import unittest
 import os
 from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+def generateString(length, alphabet=string.ascii_letters + string.digits + string.punctuation):
+    return(''.join([random.choice(alphabet) for i in range(length)]))
 
 # test utilities for converting arrays of fixed-length strings
 # to arrays of characters (with an extra dimension), and vice-versa.
 
 FILE_NAME = 'tst_stringarr.nc'
 FILE_FORMAT = 'NETCDF4_CLASSIC'
-chars = '1234567890aabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 n2 = 10; nchar = 12; nrecs = 4
 data = numpy.empty((nrecs,n2),'S'+repr(nchar))
 for nrec in range(nrecs):
     for n in range(n2):
-        data[nrec,n] = ''.join([random.choice(chars) for i in range(nchar)])
+        data[nrec,n] = generateString(nchar)
 
 class StringArrayTestCase(unittest.TestCase):
 
@@ -25,9 +27,14 @@ class StringArrayTestCase(unittest.TestCase):
         nc.createDimension('n2',n2)
         nc.createDimension('nchar',nchar)
         v = nc.createVariable('strings','S1',('n1','n2','nchar'))
+        v2 = nc.createVariable('strings2','S1',('n1','n2','nchar'))
+        # if _Encoding set, string array should automatically be converted
+        # to a char array and vice-versan
+        v2._Encoding = 'utf-8'
         for nrec in range(nrecs):
-            datac = stringtochar(data)
+            datac = stringtochar(data,encoding='utf-8')
             v[nrec] = datac[nrec]
+        v2[:] = data
         nc.close()
 
     def tearDown(self):
@@ -39,11 +46,15 @@ class StringArrayTestCase(unittest.TestCase):
         nc = Dataset(FILE_NAME)
         assert nc.dimensions['n1'].isunlimited() == True
         v = nc.variables['strings']
+        v2 = nc.variables['strings2']
         assert v.dtype.str[1:] in ['S1','U1']
         assert v.shape == (nrecs,n2,nchar)
+        datau = data.astype('U')
         for nrec in range(nrecs):
-            data2 = chartostring(v[nrec])
-            assert_array_equal(data2,data[nrec].astype('U'))
+            data2 = chartostring(v[nrec],encoding='utf-8')
+            assert_array_equal(data2,datau[nrec])
+        data2 = v2[:]
+        assert_array_equal(data2,datau)
         nc.close()
 
 if __name__ == '__main__':
