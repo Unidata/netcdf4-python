0f4f154d8c4e4b19c8e30e63f8884f1a7c00f3ed
Max Bohnet <bohnet@ggr-planung.de>
2017-07-07 00:11:45 +0200

    Update _netCDF4.pyx
    fix type

--- netCDF4/_netCDF4.pyx
+++ netCDF4/_netCDF4.pyx
@@ -1796,9 +1796,9 @@ references to the parent Dataset or Group.
             #diskless = False # don't raise error, instead silently ignore
             raise ValueError('diskless mode requires netcdf lib >= 4.2.1, you have %s' % __netcdf4libversion__)
         if sys.platform == 'win32':
-            bytestr = _strencode(str_filename, encoding='mbcs')
+            bytestr = _strencode(filename, encoding='mbcs')
         else:
-            bytestr = _strencode(str_filename)
+            bytestr = _strencode(filename)
 
         path = bytestr
 
