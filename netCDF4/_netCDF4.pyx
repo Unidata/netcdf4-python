4d5f8ec8be7a144bf286ee5e1a962b314073819a
Max Bohnet <bohnet@ggr-planung.de>
2017-07-06 13:48:46 +0200

    Update _netCDF4.pyx
    decode filepath with filesystemencoding or unquote url

--- netCDF4/_netCDF4.pyx
+++ netCDF4/_netCDF4.pyx
@@ -941,6 +941,7 @@ import netcdftime
 import numpy
 import weakref
 import sys
+import urllib
 import warnings
 from glob import glob
 from numpy import ma
@@ -1950,7 +1951,13 @@ open/create the Dataset. Requires netcdf >= 4.1.2"""
                 py_path = c_path[:pathlen] # makes a copy of pathlen bytes from c_string
             finally:
                 free(c_path)
-            return py_path.decode('ascii')
+                if self.disk_format.startswith('DAP'):
+                    # OpenDAP-Datasource
+                    decoded_path = urllib.parse.unquote(py_path)
+                else:
+                    # filepath on disk
+                    decoded_path = py_path.decode(sys.getfilesystemencoding())
+		return decoded_path
         ELSE:
             msg = """
 filepath method not enabled.  To enable, install Cython, make sure you have
