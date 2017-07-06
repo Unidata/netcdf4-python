f3585e48cc09c03fb7bb0f76758217b802a5a051
Max Bohnet <bohnet@ggr-planung.de>
2017-07-06 17:30:40 +0200

    Update tst_filepath.py
    enforcing unicode string for python2.7

--- test/tst_filepath.py
+++ test/tst_filepath.py
@@ -16,15 +16,20 @@ class test_filepath(unittest.TestCase):
 
     def test_filepath_with_non_ascii_characters(self):
         # create nc-file in a filepath with Non-Ascii-Characters
-        tempdir = tempfile.mkdtemp(prefix='ÄÖÜß_')
-        nc_non_ascii_file = os.path.join(tempdir, "Besançonalléestraße.nc")
-        nc_non_ascii = netCDF4.Dataset(nc_non_ascii_file, 'w')
+        tempdir = tempfile.mkdtemp(prefix=u'ÄÖÜß_')
+        filename = u"Besançonalléestraße.nc"
+        nc_non_ascii_file = os.path.join(tempdir, filename)
+        try:
+            nc_non_ascii = netCDF4.Dataset(nc_non_ascii_file, 'w')
+        except OSError:
+            msg = u'cannot create file {} in folder {}'.format(tempdir, filename)
+            raise OSError(msg)
         
         # test that no UnicodeDecodeError occur in the filepath() method
-        msg = 'original: {}\nstr_orig: {}\nfilepath: {}'.format(nc_non_ascii_file,
-                                                                str(nc_non_ascii_file), 
-                                                                nc_non_ascii.filepath())
-        assert nc_non_ascii.filepath() == str(nc_non_ascii_file), msg
+        msg = u'original: {}\nstr_orig: {}\nfilepath: {}'.format(
+            nc_non_ascii_file,
+            nc_non_ascii.filepath())
+        assert nc_non_ascii.filepath() == nc_non_ascii_file, msg
         
         # cleanup
         nc_non_ascii.close()
