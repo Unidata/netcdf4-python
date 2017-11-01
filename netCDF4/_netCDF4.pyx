cf24e0acfdbcce95738c29d0577e6216dde0fdfa
Jeff Whitaker <jswhit@fastmail.fm>
2017-11-01 10:11:20 -0600

    bump version number

--- netCDF4/_netCDF4.pyx
+++ netCDF4/_netCDF4.pyx
@@ -1,5 +1,5 @@
 """
-Version 1.3.1
+Version 1.3.2
 -------------
 - - - 
 
@@ -1003,7 +1003,7 @@ except ImportError:
     # python3: zip is already python2's itertools.izip
     pass
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 # Initialize numpy
 import posixpath
