4d1b998fe9a1125cd96205c33e821bd460421db7
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-16 10:21:46 -0300

    update appveyor

--- conda.recipe/run_test.py
+++
@@ -1,8 +0,0 @@
-import os
-import netCDF4
-
-# Run the unittests, skipping the opendap test.
-test_dir = os.path.join('test')
-os.chdir(test_dir)
-os.environ['NO_NET']='1'
-os.system('python run_all.py')
