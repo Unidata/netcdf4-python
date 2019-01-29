0eddbf3c719c553cd490b25858fc3b59a6577b59
Jeff Whitaker <jswhit@fastmail.fm>
2019-01-29 11:20:03 -0700

    remove blank lines

--- include/membuf.pyx
+++ include/membuf.pyx
@@ -18,14 +18,11 @@ cdef memview_fromptr(void *memory, size_t size):
 cdef class _MemBuf:
     cdef const void *memory
     cdef size_t size
-
     def __getbuffer__(self, Py_buffer *buf, int flags):
         PyBuffer_FillInfo(buf, self, <void *>self.memory, self.size, 1, flags)
-
     def __releasebuffer__(self, Py_buffer *buf):
         # why doesn't this do anything??
         pass
-
     def __dealloc__(self):
         free(self.memory)
 
