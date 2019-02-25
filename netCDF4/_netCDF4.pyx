91adf8c9de7b695e261e80838c2849ac88690f4c
Jeff Whitaker <jswhit@fastmail.fm>
2019-02-25 16:16:49 -0700

    fix test

--- netCDF4/_netCDF4.pyx
+++ netCDF4/_netCDF4.pyx
@@ -2131,8 +2131,7 @@ references to the parent Dataset or Group.
                 if clobber:
                     if parallel:
                         IF HAS_NC_PAR:
-                            #ierr = nc_create_par(path, NC_CLOBBER | NC_MPIIO | NC_NETCDF4, \
-                            ierr = nc_create_par(path, NC_CLOBBER | NC_MPIIO, \
+                            ierr = nc_create_par(path, NC_CLOBBER | NC_MPIIO | NC_NETCDF4, \
                                    mpicomm, mpiinfo, &grpid)
                         ELSE:
                             pass
