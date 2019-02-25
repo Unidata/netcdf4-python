fe4a87e6335878b0f5967535854288f4037dee72
Jeff Whitaker <jswhit@fastmail.fm>
2019-02-25 12:32:15 -0700

    update

--- ci/travis/build-parallel-netcdf.sh
+++ ci/travis/build-parallel-netcdf.sh
@@ -5,8 +5,8 @@ set -e
 echo "Using downloaded netCDF version ${NETCDF_VERSION} with parallel capabilities enabled"
 pushd /tmp
 wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-${NETCDF_VERSION}.tar.gz
-tar -xzvf netcdf-${NETCDF_VERSION}.tar.gz
-pushd netcdf-${NETCDF_VERSION}
+tar -xzvf netcdf-c-${NETCDF_VERSION}.tar.gz
+pushd netcdf-c-${NETCDF_VERSION}
 ./configure --prefix $NETCDF_DIR --enable-netcdf-4 --enable-shared --disable-dap  --enable-parallel
 make -j 2
 make install
