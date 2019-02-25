be76b10977181e31ef10b96d145db65772b1e0a5
Jeff Whitaker <jswhit@fastmail.fm>
2019-02-25 13:56:08 -0700

    try 4.6.2 again, add --disable-cdf5

--- ci/travis/build-parallel-netcdf.sh
+++ ci/travis/build-parallel-netcdf.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "Using downloaded netCDF version ${NETCDF_VERSION} with parallel capabilities enabled"
 pushd /tmp
-wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-${NETCDF_VERSION}.tar.gz
-tar -xzvf netcdf-${NETCDF_VERSION}.tar.gz
-pushd netcdf-${NETCDF_VERSION}
-./configure --prefix $NETCDF_DIR --enable-netcdf-4 --enable-shared --disable-dap  --enable-parallel
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-${NETCDF_VERSION}.tar.gz
+tar -xzvf netcdf-c-${NETCDF_VERSION}.tar.gz
+pushd netcdf-c-${NETCDF_VERSION}
+./configure --prefix $NETCDF_DIR --enable-netcdf-4 --enable-shared --disable-dap  --enable-parallel --disable-cdf5
 make -j 2
 make install
 popd
