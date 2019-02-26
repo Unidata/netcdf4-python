c67635d98b49515f7c70d6a393965bd5926a4d29
Jeff Whitaker <jswhit@fastmail.fm>
2019-02-26 08:58:30 -0700

    build from github master

--- ci/travis/build-parallel-netcdf.sh
+++ ci/travis/build-parallel-netcdf.sh
@@ -4,9 +4,15 @@ set -e
 
 echo "Using downloaded netCDF version ${NETCDF_VERSION} with parallel capabilities enabled"
 pushd /tmp
-wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-${NETCDF_VERSION}.tar.gz
-tar -xzvf netcdf-c-${NETCDF_VERSION}.tar.gz
-pushd netcdf-c-${NETCDF_VERSION}
+if [ ${NETCDF_VERSION} == "GITMASTER" ]; then
+   git clone http://github.com/Unidata/netcdf-c netcdf-c
+   pushd netcdf-c
+   autoreconf -i
+else
+   wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-${NETCDF_VERSION}.tar.gz
+   tar -xzvf netcdf-c-${NETCDF_VERSION}.tar.gz
+   pushd netcdf-c-${NETCDF_VERSION}
+fi
 ./configure --prefix $NETCDF_DIR --enable-netcdf-4 --enable-shared --disable-dap  --enable-parallel
 make -j 2
 make install
