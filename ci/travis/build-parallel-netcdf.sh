dc067ce97fa667eed47d0c871259e8668b5d3c7f
Jeff Whitaker <jswhit@fastmail.fm>
2019-02-26 10:27:18 -0700

    revert to trusty

--- ci/travis/build-parallel-netcdf.sh
+++ ci/travis/build-parallel-netcdf.sh
@@ -14,8 +14,8 @@ else
    pushd netcdf-c-${NETCDF_VERSION}
 fi
 # for Ubuntu xenial
-export CPPFLAGS="-I/usr/include/hdf5/openmpi"
-export LIBS="-lhdf5_openmpihl -lhdf5_openmpi -lm -lz"
+#export CPPFLAGS="-I/usr/include/hdf5/openmpi"
+#export LIBS="-lhdf5_openmpihl -lhdf5_openmpi -lm -lz"
 ./configure --prefix $NETCDF_DIR --enable-netcdf-4 --enable-shared --disable-dap --enable-parallel
 make -j 2
 make install
