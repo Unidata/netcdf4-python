c48ed2eaf773fdf2c1462f0062d8e20ce0a2ca27
jswhit <whitaker.jeffrey@gmail.com>
2021-01-21 09:08:43 -0700

    update

--- create_docs.sh
+++ create_docs.sh
@@ -1,5 +1,6 @@
 # Uses pdoc (https://github.com/mitmproxy/pdoc)
 # to create html docs from docstrings in Cython source.
-/Users/jwhitaker/.local/bin/pdoc -o 'docs' netCDF4
-# use resulting docs/netCDF4/_netCDF4.hml
+/Users/jwhitaker/.local/bin/pdoc -o 'docs' netCDF4 
+# use resulting docs/netCDF4/_netCDF4.html
+cp docs/netCDF4/_netCDF4.html docs/index.html
 
