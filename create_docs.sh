a562d9b0ca90790d8d5711d57d2b36a62da50d13
jswhit <whitaker.jeffrey@gmail.com>
2021-01-22 11:21:14 -0700

    update

--- create_docs.sh
+++ create_docs.sh
@@ -3,4 +3,8 @@
 /Users/jwhitaker/.local/bin/pdoc -o 'docs' netCDF4 
 # use resulting docs/netCDF4/_netCDF4.html
 cp docs/netCDF4/_netCDF4.html docs/index.html
+sed -i -e 's!href="../netCDF4.html!href="./index.html!g' docs/index.html
+sed -i -e 's!/../netCDF4.html!/index.html!g' docs/index.html
+sed -i -e 's!._netCDF4 API! API!g' docs/index.html
+sed -i -e 's!netCDF4</a>._netCDF4</h1>!netCDF4</a></h1>!g' docs/index.html
 
