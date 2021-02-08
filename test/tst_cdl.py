5b80fa8625e4cc603da920df33a56284cb052e46
jswhit <whitaker.jeffrey@gmail.com>
2021-02-08 16:17:18 -0700

    add test for data=True

--- test/tst_cdl.py
+++ test/tst_cdl.py
@@ -14,6 +14,23 @@ variables:
 		:_Format = "classic" ;
 }
 """
+test_ncdump2="""netcdf ubyte {
+dimensions:
+	d = 2 ;
+variables:
+	byte ub(d) ;
+		ub:_Unsigned = "true" ;
+	byte sb(d) ;
+
+// global attributes:
+		:_Format = "classic" ;
+data:
+
+ ub = 0, -1 ;
+
+ sb = -128, 127 ;
+}
+"""
 
 class Test_CDL(unittest.TestCase):
     """
@@ -21,12 +38,13 @@ class Test_CDL(unittest.TestCase):
     """
     def setUp(self):
         f=netCDF4.Dataset('ubyte.nc')
-        f.tocdl(outfile='ubyte.cdl')
+        f.tocdl(outfile='ubyte.cdl',data=True)
         f.close()
     def test_tocdl(self):
         # treated as unsigned integers.
         f=netCDF4.Dataset('ubyte.nc')
         assert(f.tocdl() == test_ncdump)
+        assert(f.tocdl(data=True) == test_ncdump2)
         f.close()
     def test_fromcdl(self):
         f1=netCDF4.Dataset.fromcdl('ubyte.cdl',ncfilename='ubyte2.nc')
@@ -35,6 +53,8 @@ class Test_CDL(unittest.TestCase):
         assert(f1.filepath() == 'ubyte2.nc')
         assert(f1.dimensions.keys() == f2.dimensions.keys())
         assert(len(f1.dimensions['d']) == len(f2.dimensions['d']))
+        assert((f1['ub'][:] == f2['ub'][:]).all())
+        assert((f1['sb'][:] == f2['sb'][:]).all())
         f1.close(); f2.close()
         os.remove('ubyte2.nc')
     def tearDown(self):
