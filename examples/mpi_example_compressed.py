650c1469ad3115bafdacfe91f5257b8de66bccb0
jswhit <whitaker.jeffrey@gmail.com>
2021-05-21 14:11:17 -0600

    update

--- examples/mpi_example_compressed.py
+++ examples/mpi_example_compressed.py
@@ -1,10 +1,10 @@
-# to run: mpirun -np 4 python mpi_example.py
+# to run: mpirun -np 4 python mpi_example_compressed.py
 import sys
 from mpi4py import MPI
 import numpy as np
 from netCDF4 import Dataset
 rank = MPI.COMM_WORLD.rank  # The process ID (integer 0-3 for 4-process run)
-nc = Dataset('parallel_test.nc', 'w', parallel=True)
+nc = Dataset('parallel_test_compressed.nc', 'w', parallel=True)
 d = nc.createDimension('dim',4)
 v = nc.createVariable('var', np.int32, 'dim', zlib=True)
 v[:] = np.arange(4)
@@ -13,6 +13,6 @@ nc.close()
 nc = Dataset('parallel_test.nc', 'a', parallel=True)
 v = nc['var']
 assert rank==v[rank]
-v.set_collective(True) # issue #1108
-v[rank]=1
+v.set_collective(True) # issue #1108 (var must be in collective mode or write will fail)
+v[rank]=2*rank
 nc.close()
