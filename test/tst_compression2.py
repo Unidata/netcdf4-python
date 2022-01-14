599ad6f981541cc90ade94c8f199df1aeed2e651
Jeff Whitaker <jswhit@fastmail.fm>
2022-01-14 16:37:23 -0700

    add test for 'granular bitgrooming' (significant_digits<0)

--- test/tst_compression2.py
+++ test/tst_compression2.py
@@ -6,7 +6,7 @@ import numpy as np
 import os, tempfile, unittest
 
 ndim = 100000
-nfiles = 5
+nfiles = 6
 files = [tempfile.NamedTemporaryFile(suffix='.nc', delete=False).name for nfile in range(nfiles)]
 array = uniform(size=(ndim,))
 nsd = 3
@@ -39,6 +39,8 @@ class CompressionTestCase(unittest.TestCase):
         write_netcdf(self.files[3],True,nsd,array)
         # compressed, lossy, with shuffle.
         write_netcdf(self.files[4],True,nsd,array,shuffle=True)
+        # compressed, lossy, with shuffle, and alternate quantization.
+        write_netcdf(self.files[5],True,-nsd,array,shuffle=True)
 
     def tearDown(self):
         # Remove the temporary files
@@ -78,7 +80,16 @@ class CompressionTestCase(unittest.TestCase):
         f = Dataset(self.files[4])
         size = os.stat(self.files[4]).st_size
         errmax = (np.abs(array-f.variables['data'][:])).max()
-        #print('compressed lossy with shuffle = ',size,' max err = ',errmax)
+        print('compressed lossy with shuffle and standard quantization = ',size,' max err = ',errmax)
+        assert(f.variables['data'].significant_digits() == nsd)
+        assert(errmax < 1.e-3)
+        assert(size < 0.24*uncompressed_size)
+        f.close()
+        # check lossy compression with shuffle and alternate quantization
+        f = Dataset(self.files[5])
+        size = os.stat(self.files[5]).st_size
+        errmax = (np.abs(array-f.variables['data'][:])).max()
+        print('compressed lossy with shuffle and alternate quantization = ',size,' max err = ',errmax)
         assert(f.variables['data'].significant_digits() == nsd)
         assert(errmax < 1.e-3)
         assert(size < 0.24*uncompressed_size)
