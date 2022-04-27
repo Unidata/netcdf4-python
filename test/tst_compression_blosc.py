de62007b4354801d3b5073bdd612fa5b6fd0dfcf
Jeff Whitaker <jswhit@fastmail.fm>
2022-04-27 07:46:50 -0600

    test all blosc filters

--- test/tst_compression_blosc.py
+++ test/tst_compression_blosc.py
@@ -4,50 +4,71 @@ from numpy.testing import assert_almost_equal
 import os, tempfile, unittest
 
 ndim = 100000
-filename1 = tempfile.NamedTemporaryFile(suffix='.nc', delete=False).name
-filename2 = tempfile.NamedTemporaryFile(suffix='.nc', delete=False).name
-array = uniform(size=(ndim,))
+filename = tempfile.NamedTemporaryFile(suffix='.nc', delete=False).name
+datarr = uniform(size=(ndim,))
 
 def write_netcdf(filename,dtype='f8',complevel=6):
     nc = Dataset(filename,'w')
     nc.createDimension('n', ndim)
     foo = nc.createVariable('data',\
+            dtype,('n'),compression=None)
+    foo_lz = nc.createVariable('data_lz',\
+            dtype,('n'),compression='blosc_lz',blosc_shuffle=2,complevel=complevel)
+    foo_lz4 = nc.createVariable('data_lz4',\
             dtype,('n'),compression='blosc_lz4',blosc_shuffle=2,complevel=complevel)
-    foo[:] = array
+    foo_lz4hc = nc.createVariable('data_lz4hc',\
+            dtype,('n'),compression='blosc_lz4hc',blosc_shuffle=2,complevel=complevel)
+    foo_zlib = nc.createVariable('data_zlib',\
+            dtype,('n'),compression='blosc_zlib',blosc_shuffle=2,complevel=complevel)
+    foo_zstd = nc.createVariable('data_zstd',\
+            dtype,('n'),compression='blosc_zstd',blosc_shuffle=2,complevel=complevel)
+    foo_lz[:] = datarr
+    foo_lz4[:] = datarr
+    foo_lz4hc[:] = datarr
+    foo_zlib[:] = datarr
+    foo_zstd[:] = datarr
     nc.close()
 
 class CompressionTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.filename1 = filename1
-        self.filename2 = filename2
-        write_netcdf(self.filename1,complevel=0) # no compression
-        write_netcdf(self.filename2,complevel=4) # with compression
+        self.filename = filename
+        write_netcdf(self.filename,complevel=4) # with compression
 
     def tearDown(self):
         # Remove the temporary files
-        os.remove(self.filename1)
-        os.remove(self.filename2)
+        os.remove(self.filename)
 
     def runTest(self):
-        uncompressed_size = os.stat(self.filename1).st_size
-        # check uncompressed data
-        f = Dataset(self.filename1)
-        size = os.stat(self.filename1).st_size
-        assert_almost_equal(array,f.variables['data'][:])
+        f = Dataset(self.filename)
+        assert_almost_equal(datarr,f.variables['data'][:])
         assert f.variables['data'].filters() ==\
         {'zlib':False,'zstd':False,'bzip2':False,'blosc':False,'shuffle':False,'complevel':0,'fletcher32':False}
-        assert_almost_equal(size,uncompressed_size)
-        f.close()
-        # check compressed data.
-        f = Dataset(self.filename2)
-        size = os.stat(self.filename2).st_size
-        assert_almost_equal(array,f.variables['data'][:])
-        dtest= {'zlib': False, 'zstd': False, 'bzip2': False, 'blosc':\
-        {'compressor': 'blosc_lz4', 'shuffle': 2, 'blocksize': 800000},\
-        'shuffle': False, 'complevel': 4, 'fletcher32': False}
-        assert f.variables['data'].filters() == dtest
-        assert(size < 0.96*uncompressed_size)
+        assert_almost_equal(datarr,f.variables['data_lz'][:])
+        dtest = {'zlib': False, 'zstd': False, 'bzip2': False, 'blosc':
+                {'compressor': 'blosc_lz', 'shuffle': 2, 'blocksize': 800000},
+                'shuffle': False, 'complevel': 4, 'fletcher32': False}
+        assert f.variables['data_lz'].filters() == dtest
+        assert_almost_equal(datarr,f.variables['data_lz4'][:])
+        dtest = {'zlib': False, 'zstd': False, 'bzip2': False, 'blosc':
+                {'compressor': 'blosc_lz4', 'shuffle': 2, 'blocksize': 800000},
+                'shuffle': False, 'complevel': 4, 'fletcher32': False}
+        assert f.variables['data_lz4'].filters() == dtest
+        assert_almost_equal(datarr,f.variables['data_lz4hc'][:])
+        dtest = {'zlib': False, 'zstd': False, 'bzip2': False, 'blosc':
+                {'compressor': 'blosc_lz4hc', 'shuffle': 2, 'blocksize': 800000},
+                'shuffle': False, 'complevel': 4, 'fletcher32': False}
+        assert f.variables['data_lz4hc'].filters() == dtest
+        assert_almost_equal(datarr,f.variables['data_zlib'][:])
+        dtest = {'zlib': False, 'zstd': False, 'bzip2': False, 'blosc':
+                {'compressor': 'blosc_zlib', 'shuffle': 2, 'blocksize': 800000},
+                'shuffle': False, 'complevel': 4, 'fletcher32': False}
+        assert f.variables['data_zlib'].filters() == dtest
+        assert_almost_equal(datarr,f.variables['data_zstd'][:])
+        dtest = {'zlib': False, 'zstd': False, 'bzip2': False, 'blosc':
+                {'compressor': 'blosc_zstd', 'shuffle': 2, 'blocksize': 800000},
+                'shuffle': False, 'complevel': 4, 'fletcher32': False}
+        assert f.variables['data_zstd'].filters() == dtest
         f.close()
 
 if __name__ == '__main__':
