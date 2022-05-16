fa5791c0c9c51441e8fafd8cdb07793243d59ab6
jswhit <whitaker.jeffrey@gmail.com>
2022-05-16 11:08:23 -0600

    update

--- src/netCDF4/__init__.py
+++ src/netCDF4/__init__.py
@@ -13,6 +13,6 @@ from ._netCDF4 import (__version__, __netcdf4libversion__, __hdf5libversion__,
 import os
 __all__ =\
 ['Dataset','Variable','Dimension','Group','MFDataset','MFTime','CompoundType','VLType','date2num','num2date','date2index','stringtochar','chartostring','stringtoarr','getlibversion','EnumType','get_chunk_cache','set_chunk_cache']
-# if HDF5_PLUGIN_PATH not set, point to plugins directory inside package
-if 'HDF5_PLUGIN_PATH' not in os.environ:
+# if HDF5_PLUGIN_PATH not set, point to package path if libh5noop.so exists there
+if 'HDF5_PLUGIN_PATH' not in os.environ and os.path.exists(os.path.join(__path__[0],'libh5noop.so')):
     os.environ['HDF5_PLUGIN_PATH']=__path__[0]
