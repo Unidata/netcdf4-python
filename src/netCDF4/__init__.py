a95f1359dbdb98ce9923c84787ed915d946427c1
Jeff Whitaker <jswhit@fastmail.fm>
2022-06-19 18:10:43 -0600

    update

--- src/netCDF4/__init__.py
+++ src/netCDF4/__init__.py
@@ -15,4 +15,5 @@ __all__ =\
 ['Dataset','Variable','Dimension','Group','MFDataset','MFTime','CompoundType','VLType','date2num','num2date','date2index','stringtochar','chartostring','stringtoarr','getlibversion','EnumType','get_chunk_cache','set_chunk_cache']
 # if HDF5_PLUGIN_PATH not set, point to package path if libh5noop.so exists there
 if 'HDF5_PLUGIN_PATH' not in os.environ and os.path.exists(os.path.join(__path__[0],'lib__nczhdf5filters.so')):
+    print('setting HDF5_PLUGIN_PATH to %s' % __path__[0])
     os.environ['HDF5_PLUGIN_PATH']=__path__[0]
