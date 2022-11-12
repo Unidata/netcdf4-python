eacd6e6a644462a47bbe7dc86a40acd802fb4525
Jeff Whitaker <jswhit@fastmail.fm>
2022-11-12 13:17:48 -0700

    add mpi4py import

--- setup.py
+++ setup.py
@@ -568,11 +568,12 @@ if 'sdist' not in sys.argv[1:] and 'clean' not in sys.argv[1:] and '--version' n
         has_cdf5_format = True
 
     # disable parallel support if mpi4py not available.
-    #try:
-    #    import mpi4py
-    #except ImportError:
-    #    has_parallel4_support = False
-    #    has_pnetcdf_support = False
+    try:
+        import mpi4py
+    except ImportError:
+        f.write('disabling mpi parallel support because mpi4py not found\n')
+        has_parallel4_support = False
+        has_pnetcdf_support = False
 
     f = open(osp.join('include', 'constants.pyx'), 'w')
     if has_rename_grp:
@@ -684,6 +685,7 @@ if 'sdist' not in sys.argv[1:] and 'clean' not in sys.argv[1:] and '--version' n
     f.close()
 
     if has_parallel4_support or has_pnetcdf_support:
+        import mpi4py
         inc_dirs.append(mpi4py.get_include())
         # mpi_incdir should not be needed if using nc-config
         # (should be included in nc-config --cflags)
