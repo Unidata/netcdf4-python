7e72caab33e6c280b9be774c92025f98d95148e6
Jeff Whitaker <jswhit@fastmail.fm>
2022-11-12 13:33:40 -0700

    update

--- setup.py
+++ setup.py
@@ -568,12 +568,12 @@ if 'sdist' not in sys.argv[1:] and 'clean' not in sys.argv[1:] and '--version' n
         has_cdf5_format = True
 
     # disable parallel support if mpi4py not available.
-    try:
-        import mpi4py
-    except:
-        f.write('disabling mpi parallel support because mpi4py not found\n')
-        has_parallel4_support = False
-        has_pnetcdf_support = False
+    #try:
+    #    import mpi4py
+    #except:
+    #    f.write('disabling mpi parallel support because mpi4py not found\n')
+    #    has_parallel4_support = False
+    #    has_pnetcdf_support = False
 
     f = open(osp.join('include', 'constants.pyx'), 'w')
     if has_rename_grp:
