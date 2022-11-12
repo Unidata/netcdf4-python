34cc9b964564ff10d8b2c6a895a50817c0080b9f
Jeff Whitaker <jswhit@fastmail.fm>
2022-11-12 16:17:06 -0700

    disable tarball step

--- setup.py
+++ setup.py
@@ -568,14 +568,12 @@ if 'sdist' not in sys.argv[1:] and 'clean' not in sys.argv[1:] and '--version' n
         has_cdf5_format = True
 
     # disable parallel support if mpi4py not available.
-    try:
-        import mpi4py
-        has_mpi4py = True
-    except ImportError:
-        f.write('disabling mpi parallel support because mpi4py not found\n')
-        has_parallel4_support = False
-        has_pnetcdf_support = False
-        has_mpi4py = False
+    #try:
+    #    import mpi4py
+    #except ImportError:
+    #    f.write('disabling mpi parallel support because mpi4py not found\n')
+    #    has_parallel4_support = False
+    #    has_pnetcdf_support = False
 
     f = open(osp.join('include', 'constants.pyx'), 'w')
     if has_rename_grp:
@@ -686,7 +684,7 @@ if 'sdist' not in sys.argv[1:] and 'clean' not in sys.argv[1:] and '--version' n
 
     f.close()
 
-    if has_mpi4py and (has_parallel4_support or has_pnetcdf_support):
+    if has_parallel4_support or has_pnetcdf_support:
         inc_dirs.append(mpi4py.get_include())
         # mpi_incdir should not be needed if using nc-config
         # (should be included in nc-config --cflags)
