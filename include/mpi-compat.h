a0c6e91a2e95bba8f23c0a8ce508da4852747c2e
Ben Boeckel <ben.boeckel@kitware.com>
2023-12-20 10:23:21 -0500

    mpi-compat: support MPI_Message on MS-MPI
    MS-MPI does not define an accurate `MPI_VERSION`, so `MPI_Message` is
    mis-detected.
    
    See: https://github.com/mpi4py/mpi4py/issues/19

--- include/mpi-compat.h
+++ include/mpi-compat.h
@@ -10,6 +10,10 @@
 
 #include <mpi.h>
 
+#ifdef MSMPI_VER
+#define PyMPI_HAVE_MPI_Message 1
+#endif
+
 #if (MPI_VERSION < 3) && !defined(PyMPI_HAVE_MPI_Message)
 typedef void *PyMPI_MPI_Message;
 #define MPI_Message PyMPI_MPI_Message
