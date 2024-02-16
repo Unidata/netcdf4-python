64faa9b968121b88d14051c3212ac8076f720049
stubbiali <subbiali@phys.ethz.ch>
2024-02-16 22:07:52 +0100

    Debug CI.

--- _build/backend.py
+++ _build/backend.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
+import logging
 import os
 from setuptools.build_meta import *
 import subprocess
 
 
+fmt = logging.Formatter("\n=== NETCDF4 BUILD DEBUG: %(message)s\n")
+hdlr = logging.StreamHandler()
+hdlr.setFormatter(fmt)
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+log.addHandler(hdlr)
+
+
 def netcdf_has_parallel_support():
     netcdf4_dir = os.environ.get("NETCDF4_DIR")
-    ncconfig = os.path.join(netcdf4_dir, "bin", "nc-config") if netcdf4_dir else "nc-config"
+    ncconfig = (
+        os.path.join(netcdf4_dir, "bin", "nc-config") if netcdf4_dir else "nc-config"
+    )
+    log.debug(f"{ncconfig = }")
     process = subprocess.run([ncconfig, "--has-parallel4"], capture_output=True)
     out = process.stdout.decode("utf-8").rstrip()
     return out == "yes"
@@ -22,4 +34,3 @@ def get_requires_for_build_wheel(config_settings=None):
 
 def get_requires_for_build_sdist(config_settings=None):
     return ["mpi4py>=3.1"] if netcdf_has_parallel_support() else []
-
