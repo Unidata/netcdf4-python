4ecdbb1929ec7fb8d144c2557ef87ce17f38cd83
jswhit <whitaker.jeffrey@gmail.com>
2024-06-14 12:24:55 -0600

    add nc_complex source files

---
+++ external/nc_complex/include/generated_fallbacks/nc_complex_version.h
@@ -0,0 +1,6 @@
+// This is a fallback header for when building the library without
+// CMake -- you probably should use CMake to auto-generate this instead
+#define NC_COMPLEX_GIT_SHA1 "unknown"
+#define NC_COMPLEX_GIT_VERSION "0.1.0"
+#define NC_COMPLEX_GIT_STATE "unknown"
+#define NC_COMPLEX_GIT_DATE "unknown"
