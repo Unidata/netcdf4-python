cb4d011bcdf751e5ecee8a300c6ffd64f1631194
Michael Niklas <mick.niklas@gmail.com>
2024-06-21 21:02:15 +0200

    import dunder vars in _netCDF4

--- src/netCDF4/_netCDF4.pyi
+++ src/netCDF4/_netCDF4.pyi
@@ -5,5 +5,12 @@ from . import (
     VLType, date2num, num2date, date2index, stringtochar, chartostring,
     stringtoarr, getlibversion, EnumType, get_chunk_cache, set_chunk_cache,
     set_alignment, get_alignment, default_fillvals, default_encoding,
-    NetCDF4MissingFeatureException, is_native_big, is_native_little
+    NetCDF4MissingFeatureException, is_native_big, is_native_little, unicode_error,
+    __version__, __netcdf4libversion__, __hdf5libversion__, __has_rename_grp__,
+    __has_nc_inq_path__, __has_nc_inq_format_extended__, __has_nc_open_mem__,
+    __has_nc_create_mem__, __has_cdf5_format__, __has_parallel4_support__,
+    __has_pnetcdf_support__, __has_parallel_support__,
+    __has_quantization_support__, __has_zstandard_support__,
+    __has_bzip2_support__, __has_blosc_support__, __has_szip_support__,
+    __has_set_alignment__, __has_ncfilter__
 )
