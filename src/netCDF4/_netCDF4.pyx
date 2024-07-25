f7b87f5c5753358b0a82ed00bc23074101bba773
Randall Pittman <pittmara@oregonstate.edu>
2024-07-25 08:58:30 -0700

    [docs] Add link to auth page on rc files. Incidentally pdoc re-inserted missing date2index and date2num stuff.

--- src/netCDF4/_netCDF4.pyx
+++ src/netCDF4/_netCDF4.pyx
@@ -1323,6 +1323,8 @@ def rc_get(key):
 **```rc_get(key)```**
 
 Returns the internal netcdf-c rc table value corresponding to key.
+See <https://docs.unidata.ucar.edu/netcdf-c/current/auth.html>
+for more information on rc files and values.
     """
     cdef int ierr
     cdef char *keyc
@@ -1345,7 +1347,9 @@ def rc_set(key, value):
 **```rc_set(key, value)```**
 
 Sets the internal netcdf-c rc table value corresponding to key.
-    """
+See <https://docs.unidata.ucar.edu/netcdf-c/current/auth.html>
+for more information on rc files and values.
+"""
     cdef int ierr
     cdef char *keyc
     cdef char *valuec
