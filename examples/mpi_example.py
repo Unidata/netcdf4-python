ac8c6fa81048e309e690e0d11c119ac296b479d2
Randall Pittman <pittmara@oregonstate.edu>
2024-08-23 14:25:28 -0700

    Don't bother with TypeGuard

--- examples/mpi_example.py
+++ examples/mpi_example.py
@@ -1,34 +1,23 @@
 # to run: mpirun -np 4 python mpi_example.py
 import sys
-from typing import TYPE_CHECKING
 from mpi4py import MPI
 import numpy as np
 from netCDF4 import Dataset
-from typing_extensions import TypeGuard
-if TYPE_CHECKING:
-    from netCDF4 import Format as NCFormat
-else:
-    NCFormat = object
 
-
-def is_nc_format(fmt: str) -> TypeGuard[NCFormat]:
-    return fmt in {
-        'NETCDF4',
-        'NETCDF4_CLASSIC',
-        'NETCDF3_CLASSIC',
-        'NETCDF3_64BIT_OFFSET',
-        'NETCDF3_64BIT_DATA'
-    }
 
 nc_format = 'NETCDF4_CLASSIC' if len(sys.argv) < 2 else sys.argv[1]
-if not is_nc_format(nc_format):
-        raise ValueError("Invalid file format")
 
 rank = MPI.COMM_WORLD.rank  # The process ID (integer 0-3 for 4-process run)
 if rank == 0:
     print('Creating file with format {}'.format(nc_format))
-nc = Dataset('parallel_test.nc', 'w', parallel=True, comm=MPI.COMM_WORLD,
-    info=MPI.Info(), format=nc_format)
+nc = Dataset(
+    "parallel_test.nc",
+    "w",
+    parallel=True,
+    comm=MPI.COMM_WORLD,
+    info=MPI.Info(),
+    format=nc_format,   # type: ignore  # we'll assume it's OK
+)
 # below should work also - MPI_COMM_WORLD and MPI_INFO_NULL will be used.
 #nc = Dataset('parallel_test.nc', 'w', parallel=True)
 d = nc.createDimension('dim',4)
