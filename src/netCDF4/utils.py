c05ac1049fa57d090faabb13d3bc4d1e5044980b
Jeffrey Whitaker <jwhitaker@Jeffreys-MacBook-Air.local>
2025-05-09 10:07:20 -0600

    update

--- src/netCDF4/utils.py
+++ src/netCDF4/utils.py
@@ -426,6 +426,7 @@ Boolean array must have the same shape as the data along this dimension."""
 
         #    ITERABLE    #
         elif np.iterable(e) and np.array(e).dtype.kind in 'i':  # Sequence of integers
+            print(e, sdim)
             start[...,i] = np.apply_along_axis(lambda x: e*x, i, np.ones(sdim[:-1]))
             indices[...,i] = np.apply_along_axis(lambda x: np.arange(sdim[i])*x, i, np.ones(sdim[:-1], int))
 
