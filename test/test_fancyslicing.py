2d5b802d9ca61e586d24942f65fd9d7241f9da35
Jeffrey Whitaker <jwhitaker@Jeffreys-MacBook-Air.local>
2025-08-02 12:39:49 -0600

    debug print

--- test/test_fancyslicing.py
+++ test/test_fancyslicing.py
@@ -144,6 +144,9 @@ class VariablesTestCase(unittest.TestCase):
 
         # slicing with all False booleans (PR #1197)
         iby[:] = False
+        print(ibx)
+        print(iby)
+        print(ibz)
         data = v[ibx,iby,ibz]
         assert data.size == 0 
 
