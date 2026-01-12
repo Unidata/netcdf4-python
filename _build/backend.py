53f093bde6e308997cc819d532ac22bbf2d1a90c
stubbiali <subbiali@phys.ethz.ch>
2026-01-12 17:12:19 +0100

    Try using mpi4py<4.1

--- _build/backend.py
+++ _build/backend.py
@@ -25,12 +25,12 @@ def netcdf_has_parallel_support():
 
 
 def get_requires_for_build_editable(config_settings=None):
-    return ["mpi4py>=3.1"] if netcdf_has_parallel_support() else []
+    return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
 
 
 def get_requires_for_build_wheel(config_settings=None):
-    return ["mpi4py>=3.1"] if netcdf_has_parallel_support() else []
+    return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
 
 
 def get_requires_for_build_sdist(config_settings=None):
-    return ["mpi4py>=3.1"] if netcdf_has_parallel_support() else []
+    return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
