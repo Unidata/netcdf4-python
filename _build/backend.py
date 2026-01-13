cc5353709c26d68714b900c0e37b11e18a4cf1c4
stubbiali <subbiali@phys.ethz.ch>
2026-01-13 11:07:05 +0100

    Sort alphabetically

--- _build/backend.py
+++ _build/backend.py
@@ -28,9 +28,9 @@ def get_requires_for_build_editable(config_settings=None):
     return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
 
 
-def get_requires_for_build_wheel(config_settings=None):
+def get_requires_for_build_sdist(config_settings=None):
     return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
 
 
-def get_requires_for_build_sdist(config_settings=None):
+def get_requires_for_build_wheel(config_settings=None):
     return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
