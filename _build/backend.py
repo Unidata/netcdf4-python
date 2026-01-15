b3635d7c3bc0f78ad125d1d9be66ca8923924656
stubbiali <subbiali@phys.ethz.ch>
2026-01-15 08:51:11 +0100

    Use utilities from setup.py to check if netcdf has parallel support

--- _build/backend.py
+++ _build/backend.py
@@ -1,36 +1,16 @@
 # -*- coding: utf-8 -*-
-import logging
-import os
 from setuptools.build_meta import *
-import subprocess
 
-
-fmt = logging.Formatter("\n=== NETCDF4 BUILD DEBUG: %(message)s\n")
-hdlr = logging.StreamHandler()
-hdlr.setFormatter(fmt)
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-log.addHandler(hdlr)
-
-
-def netcdf_has_parallel_support():
-    netcdf4_dir = os.environ.get("NETCDF4_DIR")
-    ncconfig = (
-        os.path.join(netcdf4_dir, "bin", "nc-config") if netcdf4_dir else "nc-config"
-    )
-    log.debug(f"{ncconfig = }")
-    process = subprocess.run([ncconfig, "--has-parallel4"], capture_output=True)
-    out = process.stdout.decode("utf-8").rstrip()
-    return out == "yes"
+import utils
 
 
 def get_requires_for_build_editable(config_settings=None):
-    return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
+    return ["mpi4py>=3.1,<4.1"] if utils.netcdf4_has_parallel_support() else []
 
 
 def get_requires_for_build_sdist(config_settings=None):
-    return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
+    return ["mpi4py>=3.1,<4.1"] if utils.netcdf4_has_parallel_support() else []
 
 
 def get_requires_for_build_wheel(config_settings=None):
-    return ["mpi4py>=3.1,<4.1"] if netcdf_has_parallel_support() else []
+    return ["mpi4py>=3.1,<4.1"] if utils.netcdf4_has_parallel_support() else []
