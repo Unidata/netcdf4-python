7a59bfd39aa8f6579a58760d5cd04f6d1de50d8a
stubbiali <subbiali@phys.ethz.ch>
2026-01-15 09:04:48 +0100

    Try removing upper constraint on mpi4py

--- _build/backend.py
+++ _build/backend.py
@@ -10,12 +10,12 @@ import utils
 
 
 def get_requires_for_build_editable(config_settings=None):
-    return ["mpi4py>=3.1,<4.1"] if utils.netcdf4_has_parallel_support() else []
+    return ["mpi4py>=3.1"] if utils.netcdf4_has_parallel_support() else []
 
 
 def get_requires_for_build_sdist(config_settings=None):
-    return ["mpi4py>=3.1,<4.1"] if utils.netcdf4_has_parallel_support() else []
+    return ["mpi4py>=3.1"] if utils.netcdf4_has_parallel_support() else []
 
 
 def get_requires_for_build_wheel(config_settings=None):
-    return ["mpi4py>=3.1,<4.1"] if utils.netcdf4_has_parallel_support() else []
+    return ["mpi4py>=3.1"] if utils.netcdf4_has_parallel_support() else []
